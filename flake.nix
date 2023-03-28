0e122bc60307b2d7bae9dc7b599c37d2127e6f43
mlyxshi <mlyxdev@gmail.com>
2023-03-28 14:56:43 -0700

    update

--- flake.nix
+++ flake.nix
@@ -77,7 +77,6 @@
 
       devShells.aarch64-darwin.wrangler = import ./shells/wrangler.nix { pkgs = nixpkgs.legacyPackages."aarch64-darwin"; };
 
-
       packages.x86_64-linux.default = self.nixosConfigurations."kexec-x86_64".config.system.build.test;
       packages.x86_64-linux.test0 = self.nixosConfigurations."kexec-x86_64".config.system.build.test0;
 
