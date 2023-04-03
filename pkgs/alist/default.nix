814f630094884c74e817e477f15b714d3ed4e8d3
mlyxshi <mlyxdev@gmail.com>
2023-04-03 11:54:24 -0700

    alist: 3.15.0 -> 3.15.0
    Diff: https://github.com/alist-org/alist/compare/None...v3.15.0

--- pkgs/alist/default.nix
+++ pkgs/alist/default.nix
@@ -5,7 +5,7 @@ stdenv.mkDerivation rec {
   version = "3.15.0";
   src = fetchzip {
     url = "https://github.com/alist-org/alist/releases/download/v${version}/alist-linux-musl-arm64.tar.gz";
-    sha256 = "sha256-ph/OgpQfBBgDxvggzXQqjq3/FuxQSGVxBANyCcRdl0c=";
+    hash = "";
   };
 
   installPhase = ''
