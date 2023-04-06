1bb67203c23e262ca7f3eb9146f67dcf0273ce6f
mlyxshi <mlyxdev@gmail.com>
2023-04-06 13:15:49 -0700

    update

--- modules/container/auto-bangumi.nix
+++ modules/container/auto-bangumi.nix
@@ -13,7 +13,7 @@
         "PGID" = "1000";
         "AB_DOWNLOADER_USERNAME"="admin";
         "AB_DOWNLOADER_HOST"="127.0.0.1:8080";
-        "AB_DEBUG_MODE"="True";
+        # "AB_DEBUG_MODE"="True";
         "AB_INTERVAL_TIME"="1";
       };
       environmentFiles = [
