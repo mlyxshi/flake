d3e89ccf4a280953d4a911e7b6a71a377f8b43ec
mlyxshi <mlyxdev@gmail.com>
2023-04-06 23:43:32 -0700

    update

--- modules/services/bangumi/jellyfin.nix
+++ modules/services/bangumi/jellyfin.nix
@@ -11,7 +11,7 @@
       http = {
         routers.jellyfin = {
           rule = "Host(`jellyfin.${config.networking.domain}`)";
-          entryPoints = [ "websecure" ];
+          entryPoints = [ "web" ];
           service = "jellyfin";
         };
 
