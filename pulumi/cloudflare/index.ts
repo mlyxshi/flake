bdb2e04a88dfa0e9ea2b32ee68882f880a54ccad
mlyxshi <mlyxdev@gmail.com>
2023-04-20 12:24:07 -0700

    test

--- pulumi/cloudflare/index.ts
+++ pulumi/cloudflare/index.ts
@@ -2,7 +2,7 @@ import * as pulumi from "@pulumi/pulumi";
 import * as cloudflare from "@pulumi/cloudflare";
 
 const record = new cloudflare.Record("sample-record", {
-  name: "my-record",
+  name: "my-record1",
   zoneId: "9635f891a392db45a76bca59db689db0",
   type: "A",
   value: "11.111.11.1",
