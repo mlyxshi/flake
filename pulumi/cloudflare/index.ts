ff58231cfc6bef850762c91c51fed6bc6c1f52be
mlyxshi <mlyxdev@gmail.com>
2023-04-20 12:30:50 -0700

    refactor1

--- pulumi/cloudflare/index.ts
+++ pulumi/cloudflare/index.ts
@@ -5,7 +5,7 @@ const record = new cloudflare.Record("sample-record", {
   name: "my-record",
   zoneId: "9635f891a392db45a76bca59db689db0",
   type: "A",
-  value: "11.111.11.3",
+  value: "4.111.11.3",
   ttl: 1,
   proxied: false,
 });
