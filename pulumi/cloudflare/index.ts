04f9ee23c33b7301d96cb5c71b76b7948b0f2d2b
mlyxshi <mlyxdev@gmail.com>
2023-04-20 12:27:31 -0700

    refactor

--- pulumi/cloudflare/index.ts
+++ pulumi/cloudflare/index.ts
@@ -2,10 +2,10 @@ import * as pulumi from "@pulumi/pulumi";
 import * as cloudflare from "@pulumi/cloudflare";
 
 const record = new cloudflare.Record("sample-record", {
-  name: "my-record1",
+  name: "my-record",
   zoneId: "9635f891a392db45a76bca59db689db0",
   type: "A",
-  value: "11.111.11.1",
+  value: "11.111.11.2",
   ttl: 1,
   proxied: false,
 });
