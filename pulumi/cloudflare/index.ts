a729bf2742f3f3b4243cceaea9d0a410b197ed92
mlyxshi <mlyxdev@gmail.com>
2023-04-20 12:37:31 -0700

    pulumi1

--- pulumi/cloudflare/index.ts
+++ pulumi/cloudflare/index.ts
@@ -1,11 +1,13 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as cloudflare from "@pulumi/cloudflare";
 
+const zoneId = "9635f891a392db45a76bca59db689db0";
+
 const record = new cloudflare.Record("sample-record", {
-  name: "my-record",
-  zoneId: "9635f891a392db45a76bca59db689db0",
+  name: "alert",
+  zoneId: zoneId,
   type: "A",
-  value: "4.111.11.3",
+  value: "140.238.198.209",
   ttl: 1,
   proxied: false,
 });
