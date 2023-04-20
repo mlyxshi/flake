4d1bbfa41e47b91c6adfbbaea720681200012d1d
mlyxshi <mlyxdev@gmail.com>
2023-04-20 13:08:43 -0700

    pulumi7

--- pulumi/cloudflare/index.ts
+++ pulumi/cloudflare/index.ts
@@ -3,21 +3,21 @@ import * as cloudflare from "@pulumi/cloudflare";
 
 const zoneId = "9635f891a392db45a76bca59db689db0";
 
-new cloudflare.Record("alert", {
-  name: "alert",
-  zoneId: zoneId,
-  type: "A",
-  value: "140.238.198.209",
-  ttl: 1,
-  proxied: false,
-});
+const dns: Record<string, string[]> = {
+    //"130.61.171.180": ["de", "transmission", "transmission-index"]
+    "140.238.198.209": ["au", "alert"]
+}
 
-
-new cloudflare.Record("au", {
-    name: "au",
-    zoneId: zoneId,
-    type: "A",
-    value: "140.238.198.209",
-    ttl: 1,
-    proxied: false,
+Object.keys(dns).forEach(ip => {
+    const records = dns[ip];
+    records.forEach(record => {
+        new cloudflare.Record(record, {
+            name: record,
+            zoneId: zoneId,
+            type: "A",
+            value: ip,
+            ttl: 1,
+            proxied: false,
+        });
+    });
 });
