16c22588af3ec69142c953f2b9b3cb4b58bfb3e9
mlyxshi <mlyxdev@gmail.com>
2023-04-21 20:37:12 -0700

    update

--- host/oracle/aarch64/sw.nix
+++ host/oracle/aarch64/sw.nix
@@ -4,7 +4,4 @@
     self.nixosModules.container.change-detection
     self.nixosModules.container.rsshub
   ];
-
-  # change-detection do not work with nftables
-  networking.nftables.enable = lib.mkForce false;
 }
