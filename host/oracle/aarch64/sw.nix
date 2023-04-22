2d25812047451258f83512693d6fbc58d88b2b4d
mlyxshi <mlyxdev@gmail.com>
2023-04-22 12:08:47 -0700

    update

--- host/oracle/aarch64/sw.nix
+++ host/oracle/aarch64/sw.nix
@@ -1,7 +1,8 @@
 { self, config, pkgs, lib, ... }: {
   imports = [
     self.nixosModules.container.podman
-    self.nixosModules.container.change-detection
     self.nixosModules.container.rsshub
+
+    self.nixosModules.services.change-detection
   ];
 }
