857c3ebe6322b72f8d57dc7a7497ae20bafdd3cb
mlyxshi <mlyxdev@gmail.com>
2023-04-24 14:41:39 -0700

    update

--- modules/services/transmission/jellyfin.nix
+++ modules/services/transmission/jellyfin.nix
@@ -1,11 +1,40 @@
 # https://jellyfin-plugin-bangumi.pages.dev/repository.json
 { pkgs, lib, config, ... }: {
 
+  sops.secrets.restic-env = { };
+
   services.jellyfin = {
     enable = true;
     user = "transmission";
     group = "transmission";
   };
+
+  systemd.services.jellyfin-data-init = {
+    after = [ "transmission.service" ];
+    before = [ "jellyfin.service" ];
+    unitConfig.ConditionPathExists = "!%S/jellyfin";
+    environment.RESTIC_CACHE_DIR = "%C/restic";
+    serviceConfig.EnvironmentFile = config.sops.secrets.restic-env.path;
+    serviceConfig.ExecSearchPath = "${pkgs.restic}/bin";
+    serviceConfig.ExecStart = "restic restore latest --path %S/jellyfin  --target /";
+    wantedBy = [ "multi-user.target" ];
+  };
+
+  systemd.services.jellyfin-backup = {
+    environment.RESTIC_CACHE_DIR = "%C/restic";
+    serviceConfig = {
+      Type = "oneshot";
+      EnvironmentFile = config.sops.secrets.restic-env.path;
+      ExecSearchPath = "${pkgs.restic}/bin";
+      ExecStart = [
+        "restic backup %S/jellyfin"
+        "restic forget --prune --keep-last 2"
+        "restic check"
+      ];
+    };
+    startAt = "07:00";
+  };
+
 
   services.traefik = {
     dynamicConfigOptions = {
