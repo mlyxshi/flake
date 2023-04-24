bcc2b513886c467ddbf62f81f22cfba995281729
mlyxshi <mlyxdev@gmail.com>
2023-04-23 20:11:37 -0700

    update

--- pulumi/cloudflare/index.ts
+++ pulumi/cloudflare/index.ts
@@ -1,7 +1,5 @@
 import * as cloudflare from "@pulumi/cloudflare";
 
-const zoneId = "9635f891a392db45a76bca59db689db0";
-
 const dns: Record<string, string[]> = {
     "130.61.171.180": ["de"],
     "140.238.198.209": ["au", "alert", "metric"],
@@ -21,7 +19,7 @@ Object.keys(dns).forEach(ip => {
     records.forEach(record => {
         new cloudflare.Record(record, {
             name: record,
-            zoneId: zoneId,
+            zoneId: "9635f891a392db45a76bca59db689db0",
             type: "A",
             value: ip,
             ttl: 1,
