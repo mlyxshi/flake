5d5c007dc538669cd4c6adb495f33a91d00a19a0
mlyxshi <mlyxdev@gmail.com>
2023-04-25 03:27:42 -0700

    update

--- modules/container/rsshub.nix
+++ modules/container/rsshub.nix
@@ -2,7 +2,7 @@
 
   virtualisation.oci-containers.containers = {
     "rsshub" = {
-      image = "ghcr.io/diygod/rsshub:chromium-bundled";
+      image = "ghcr.io/diygod/rsshub:latest";
       extraOptions = lib.concatMap (x: [ "--label" x ]) [
         "io.containers.autoupdate=registry"
         "traefik.enable=true"
