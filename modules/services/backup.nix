c2971b6b3010dc3cee2354c86e717e1efe19e5a7
mlyxshi <mlyxdev@gmail.com>
2023-04-27 20:17:20 -0700

    update

--- modules/services/backup.nix
+++ modules/services/backup.nix
@@ -13,37 +13,38 @@ in
     {
       sops.secrets.restic-env = { };
     }
+  ] ++ lib.mapAttrsToList
+    (name: value:
 
-
-    (lib.mkIf cfg.jellyfin {
-      systemd.services.jellyfin-init = {
-        after = [ "network-online.target" ];
-        before = [ "podman-jellyfin.service" ];
-        unitConfig.ConditionPathExists = "!%S/jellyfin";
-        environment.RESTIC_CACHE_DIR = "%C/restic";
-        serviceConfig.EnvironmentFile = config.sops.secrets.restic-env.path;
-        serviceConfig.ExecSearchPath = "${pkgs.restic}/bin";
-        serviceConfig.ExecStart = "restic restore latest --path %S/jellyfin  --target /";
-        wantedBy = [ "multi-user.target" ];
-      };
-
-      systemd.services.jellyfin-backup = {
-        environment.RESTIC_CACHE_DIR = "%C/restic";
-        serviceConfig = {
-          Type = "oneshot";
-          EnvironmentFile = config.sops.secrets.restic-env.path;
-          ExecSearchPath = "${pkgs.restic}/bin";
-          ExecStart = [
-            "restic backup %S/jellyfin"
-            "restic forget --prune --keep-last 2"
-            "restic check"
-          ];
+      (lib.mkIf cfg.name {
+        systemd.services."${name}-init" = {
+          after = [ "network-online.target" ];
+          before = [ "podman-${name}.service" ];
+          unitConfig.ConditionPathExists = "!%S/${name}";
+          environment.RESTIC_CACHE_DIR = "%C/restic";
+          serviceConfig.EnvironmentFile = config.sops.secrets.restic-env.path;
+          serviceConfig.ExecSearchPath = "${pkgs.restic}/bin";
+          serviceConfig.ExecStart = "restic restore latest --path %S/${name}  --target /";
+          wantedBy = [ "multi-user.target" ];
         };
-        startAt = "05:00";
-      };
-    })
 
-  ];
+        systemd.services."${name}-backup" = {
+          environment.RESTIC_CACHE_DIR = "%C/restic";
+          serviceConfig = {
+            Type = "oneshot";
+            EnvironmentFile = config.sops.secrets.restic-env.path;
+            ExecSearchPath = "${pkgs.restic}/bin";
+            ExecStart = [
+              "restic backup %S/${name}"
+              "restic forget --prune --keep-last 2"
+              "restic check"
+            ];
+          };
+          startAt = value;
+        };
+      })
+    )
+    servicelist;
 
 
 
