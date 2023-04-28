7ab7c8848d4df2c7d4b5776ea098fdebfb4c3bfa
mlyxshi <mlyxdev@gmail.com>
2023-04-27 20:08:55 -0700

    update

--- modules/services/backup.nix
+++ modules/services/backup.nix
@@ -1,46 +1,18 @@
 { pkgs, lib, config, ... }:
 let
   cfg = config.backup;
-  service = [ "changedetection" "jellyfin" ];
-  time = [ "08:00" "05:00" ];
+  servicelist = {
+    changedetection = "08:00";
+    jellyfin = "05:00";
+  };
 in
 {
-  # jellyfin = lib.mkEnableOption "jellyfin";
-  options.backup = lib.genAttrs service (x: lib.mkEnableOption x);
+  options.backup = lib.genAttrs (builtins.attrNames servicelist) (x: lib.mkEnableOption x);
 
   config = lib.mkMerge [
     {
       sops.secrets.restic-env = { };
     }
-
-    (lib.mkIf cfg.changedetection {
-      systemd.services."changedetection-init" = {
-        after = [ "network-online.target" ];
-        before = [ "podman-changedetection.service" ];
-        unitConfig.ConditionPathExists = "!%S/changedetection";
-        environment.RESTIC_CACHE_DIR = "%C/restic";
-        serviceConfig.EnvironmentFile = config.sops.secrets.restic-env.path;
-        serviceConfig.ExecSearchPath = "${pkgs.restic}/bin";
-        serviceConfig.ExecStart = "restic restore latest --path %S/changedetection  --target /";
-        wantedBy = [ "multi-user.target" ];
-      };
-
-      systemd.services."changedetection-backup" = {
-        environment.RESTIC_CACHE_DIR = "%C/restic";
-        serviceConfig = {
-          Type = "oneshot";
-          EnvironmentFile = config.sops.secrets.restic-env.path;
-          ExecSearchPath = "${pkgs.restic}/bin";
-          ExecStart = [
-            "restic backup %S/changedetection"
-            "restic forget --prune --keep-last 2"
-            "restic check"
-          ];
-        };
-        startAt = "08:00";
-      };
-    })
-
 
 
     (lib.mkIf cfg.jellyfin {
