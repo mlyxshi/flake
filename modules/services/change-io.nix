a0fe405e1fb2debd0329c371fbd66ed62b7bd362
mlyxshi <mlyxdev@gmail.com>
2023-04-27 18:16:11 -0700

    update

--- modules/services/change-io.nix
+++ modules/services/change-io.nix
@@ -1,5 +1,33 @@
 { pkgs, lib, config, ... }: {
+  sops.secrets.restic-env = { };
+  
   services.changedetection-io.enable = true;
+
+  systemd.services.changedetection-io-init = {
+    after = [ "network-online.target" ];
+    before = [ "changedetection-io.service" ];
+    unitConfig.ConditionPathExists = "!%S/changedetection-io";
+    environment.RESTIC_CACHE_DIR = "%C/restic";
+    serviceConfig.EnvironmentFile = config.sops.secrets.restic-env.path;
+    serviceConfig.ExecSearchPath = "${pkgs.restic}/bin";
+    serviceConfig.ExecStart = "restic restore latest --path %S/changedetection-io  --target /";
+    wantedBy = [ "multi-user.target" ];
+  };
+
+  systemd.services.changedetection-io-backup = {
+    environment.RESTIC_CACHE_DIR = "%C/restic";
+    serviceConfig = {
+      Type = "oneshot";
+      EnvironmentFile = config.sops.secrets.restic-env.path;
+      ExecSearchPath = "${pkgs.restic}/bin";
+      ExecStart = [
+        "restic backup %S/changedetection-io"
+        "restic forget --prune --keep-last 2"
+        "restic check"
+      ];
+    };
+    startAt = "08:00";
+  };
 
   services.traefik = {
     dynamicConfigOptions = {
