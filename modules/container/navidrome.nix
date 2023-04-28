22d0d5067887c3e217675a0c7c6e902075abaf10
mlyxshi <mlyxdev@gmail.com>
2023-04-28 11:17:37 -0700

    refactor

--- modules/container/navidrome.nix
+++ modules/container/navidrome.nix
@@ -2,6 +2,10 @@
 
   virtualisation.oci-containers.containers.navidrome = {
     image = "ghcr.io/navidrome/navidrome";
+    volumes = [
+      "/var/lib/navidrome:/data"
+      "/var/lib/music:/music"
+    ];
     extraOptions = lib.concatMap (x: [ "--label" x ]) [
       "io.containers.autoupdate=registry"
       "traefik.enable=true"
