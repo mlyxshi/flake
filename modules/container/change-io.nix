3e7b6431711c4cb4f145a60798d704f74142b7d4
mlyxshi <mlyxdev@gmail.com>
2023-04-27 19:18:42 -0700

    update

--- modules/container/change-io.nix
+++ modules/container/change-io.nix
@@ -1,6 +1,8 @@
 { config, pkgs, lib, ... }: {
 
-  sops.secrets.restic-env = { };
+  imports=[
+    ./helpers.nix { name = "changedetection"; }
+  ];
 
   virtualisation.oci-containers.containers.changedetection = {
     image = "ghcr.io/dgtlmoon/changedetection.io";
@@ -17,29 +19,5 @@
 
   systemd.services.podman-changedetection.serviceConfig.StateDirectory = "changedetection";
 
-  systemd.services.changedetection-init = {
-    after = [ "network-online.target" ];
-    before = [ "podman-changedetection.service" ];
-    unitConfig.ConditionPathExists = "!%S/changedetection";
-    environment.RESTIC_CACHE_DIR = "%C/restic";
-    serviceConfig.EnvironmentFile = config.sops.secrets.restic-env.path;
-    serviceConfig.ExecSearchPath = "${pkgs.restic}/bin";
-    serviceConfig.ExecStart = "restic restore latest --path %S/changedetection  --target /";
-    wantedBy = [ "multi-user.target" ];
-  };
 
-  systemd.services.changedetection-backup = {
-    environment.RESTIC_CACHE_DIR = "%C/restic";
-    serviceConfig = {
-      Type = "oneshot";
-      EnvironmentFile = config.sops.secrets.restic-env.path;
-      ExecSearchPath = "${pkgs.restic}/bin";
-      ExecStart = [
-        "restic backup %S/changedetection"
-        "restic forget --prune --keep-last 2"
-        "restic check"
-      ];
-    };
-    startAt = "08:00";
-  };
 }
---
+++ modules/container/helper.nix
@@ -0,0 +1,30 @@
+  
+name:{ config, pkgs, lib, ... }: {  
+  sops.secrets.restic-env = { };
+
+  systemd.services."${name}-init" = {
+    after = [ "network-online.target" ];
+    before = [ "podman-${name}.service" ];
+    unitConfig.ConditionPathExists = "!%S/${name}";
+    environment.RESTIC_CACHE_DIR = "%C/restic";
+    serviceConfig.EnvironmentFile = config.sops.secrets.restic-env.path;
+    serviceConfig.ExecSearchPath = "${pkgs.restic}/bin";
+    serviceConfig.ExecStart = "restic restore latest --path %S/${name}  --target /";
+    wantedBy = [ "multi-user.target" ];
+  };
+
+  systemd.services."${name}-backup" = {
+    environment.RESTIC_CACHE_DIR = "%C/restic";
+    serviceConfig = {
+      Type = "oneshot";
+      EnvironmentFile = config.sops.secrets.restic-env.path;
+      ExecSearchPath = "${pkgs.restic}/bin";
+      ExecStart = [
+        "restic backup %S/${name}"
+        "restic forget --prune --keep-last 2"
+        "restic check"
+      ];
+    };
+    startAt = "08:00";
+  };
+}
