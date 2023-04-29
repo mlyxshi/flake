d9ee05fdd65ca8d0cf320e5d07ebd01afc1fc12a
mlyxshi <mlyxdev@gmail.com>
2023-04-29 07:19:37 -0700

    update

--- modules/default.nix
+++ modules/default.nix
@@ -1,18 +1,7 @@
-{ lib }:
+lib:
 let
   ls = dir: builtins.attrNames (builtins.readDir dir);
   pureName = pathList: map (path: lib.strings.removeSuffix ".nix" path) pathList;
   mkModules = dir: lib.genAttrs (pureName (ls ./${dir})) (file: if lib.sources.pathIsDirectory ./${dir}/${file} then import ./${dir}/${file} else import ./${dir}/${file}.nix);
 in
 lib.genAttrs (ls ./.) (dir: mkModules dir)
-# {
-#   network = mkModules "network";
-#   fileSystem = mkModules "fileSystem";
-#   darwin = mkModules "darwin";
-#   nixos = mkModules "nixos";
-#   settings = mkModules "settings";
-#   containers = mkModules "containers";
-#   services =  mkModules "services";
-# }
-
-
