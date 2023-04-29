7864b77368769fd879b3fa0c43201fdd70cf4760
mlyxshi <mlyxdev@gmail.com>
2023-04-29 14:26:14 -0700

    refactor

--- flake.nix
+++ flake.nix
@@ -27,12 +27,14 @@
       azure-x64-serverlist = pureName (ls ./host/azure/x86_64);
     in
     {
-      overlays.default = final: prev:
+
+      overlays.default =
+        final: prev:
         let
           dirContents = builtins.readDir ./pkgs;
           names = builtins.attrNames dirContents;
         in
-        prev.lib.genAttrs names (name: final.callPackage ./pkgs/${name} { });
+        prev.lib.genAttrs names (name: final.callPackage (./pkgs + "/${name}") { });
       nixosModules = mkFileHierarchyAttrset "." "modules";
       darwinConfigurations.M1 = import ./host/M1 { inherit self nixpkgs darwin home-manager; };
       nixosConfigurations = {
