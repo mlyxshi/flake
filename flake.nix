9474181cad9a79cb0dc4c63e16d1cf8c999764ae
mlyxshi <mlyxdev@gmail.com>
2023-04-29 14:31:13 -0700

    refactor

--- flake.nix
+++ flake.nix
@@ -25,11 +25,12 @@
       oracle-arm64-serverlist = pureName (ls ./host/oracle/aarch64);
       oracle-x64-serverlist = pureName (ls ./host/oracle/x86_64);
       azure-x64-serverlist = pureName (ls ./host/azure/x86_64);
+      aaa= pureName (ls ./pkgs);
     in
     {
 
       overlays.default =
-        final: prev: prev.lib.genAttrs ["transmission" "nodestatus-client"] (name: final.callPackage ./pkgs/${name} { });
+        final: prev: prev.lib.genAttrs aaa (name: final.callPackage ./pkgs/${name} { });
       nixosModules = mkFileHierarchyAttrset "." "modules";
       darwinConfigurations.M1 = import ./host/M1 { inherit self nixpkgs darwin home-manager; };
       nixosConfigurations = {
