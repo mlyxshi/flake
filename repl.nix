71d95802ade701d8dd4ce790a75ec562d141a948
mlyxshi <mlyxdev@gmail.com>
2023-04-29 13:47:38 -0700

    update

--- repl.nix
+++ repl.nix
@@ -3,22 +3,10 @@ let
   nixpkgs = flake.inputs.nixpkgs;
   pkgs = import nixpkgs { system = builtins.currentSystem; };
   lib = pkgs.lib;
-  ls = dir: builtins.attrNames (builtins.readDir dir);
-  pureName = pathList: map (path: lib.strings.removeSuffix ".nix" path) pathList;
-  mkFileHierarchyAttrset = basedir: dir:
-  lib.genAttrs (pureName (ls ./${basedir}/${dir}))
-    (path:
-      if builtins.pathExists ./${basedir}/${dir}/${path}.nix
-      then import ./${basedir}/${dir}/${path}.nix
-      else if builtins.pathExists ./${basedir}/${dir}/${path}/default.nix
-      then import ./${basedir}/${dir}/${path}
-      else mkFileHierarchyAttrset "./${basedir}/${dir}" path
-    );
+  utils = import ./utils.nix lib;
 in
 {
-  inherit mkFileHierarchyAttrset;
-  inherit ls;
-  inherit pureName;
+  inherit (utils) ls pureName mkFileHierarchyAttrset;
 }
 
 // builtins
