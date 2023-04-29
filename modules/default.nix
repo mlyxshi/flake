8c1bc51258dd8bec8a293f3bde2bc836fc450c19
mlyxshi <mlyxdev@gmail.com>
2023-04-29 07:18:54 -0700

    update

--- modules/default.nix
+++ modules/default.nix
@@ -4,12 +4,15 @@ let
   pureName = pathList: map (path: lib.strings.removeSuffix ".nix" path) pathList;
   mkModules = dir: lib.genAttrs (pureName (ls ./${dir})) (file: if lib.sources.pathIsDirectory ./${dir}/${file} then import ./${dir}/${file} else import ./${dir}/${file}.nix);
 in
-{
-  network = mkModules "network";
-  fileSystem = mkModules "fileSystem";
-  darwin = mkModules "darwin";
-  nixos = mkModules "nixos";
-  settings = mkModules "settings";
-  containers = mkModules "containers";
-  services =  mkModules "services";
-}
+lib.genAttrs (ls ./.) (dir: mkModules dir)
+# {
+#   network = mkModules "network";
+#   fileSystem = mkModules "fileSystem";
+#   darwin = mkModules "darwin";
+#   nixos = mkModules "nixos";
+#   settings = mkModules "settings";
+#   containers = mkModules "containers";
+#   services =  mkModules "services";
+# }
+
+
