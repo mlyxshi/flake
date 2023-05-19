e6469a1916cc7404f97eb8a576bbf7f2dfb4afc9
mlyxshi <mlyxdev@gmail.com>
2023-05-19 12:28:48 +0800

    update

--- modules/services/shadowsocks.nix
+++ modules/services/shadowsocks.nix
@@ -8,11 +8,9 @@
       "server_port":6666,
       "method":"aes-256-gcm",
       "password":"${config.sops.placeholder.shadowsocks-pwd}",
-      "fast_open":true,
-      "mode":"tcp_and_udp"
     }
   '';
-  
+
   systemd.services.shadowsocks = {
     after = [ "network.target" ];
     wantedBy = [ "multi-user.target" ];
