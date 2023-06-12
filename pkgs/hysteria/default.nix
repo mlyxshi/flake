23e397a641a19ad513686aa36c52a08ec4d951af
mlyxshi <mlyxdev@gmail.com>
2023-06-12 13:20:45 +0800

    update hysteria

--- pkgs/hysteria/default.nix
+++ pkgs/hysteria/default.nix
@@ -7,7 +7,7 @@ buildGoModule rec {
   version = "1.3.5";
 
   src = fetchFromGitHub {
-    owner = "HyNetwork";
+    owner = "apernet";
     repo = pname;
     rev = "v${version}";
     hash = "sha256-gNAK+WOowBlEzUYX25cQxywerNlMIx7cqG1wV9iLS5s=";
@@ -31,7 +31,7 @@ buildGoModule rec {
 
   meta = with lib; {
     description = "A feature-packed proxy & relay utility optimized for lossy, unstable connections";
-    homepage = "https://github.com/HyNetwork/hysteria";
+    homepage = "https://github.com/apernet/hysteria";
     license = licenses.mit;
     platforms = platforms.linux;
     maintainers = with maintainers; [ oluceps ];
