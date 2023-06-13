fe1456f45fcd247014fad4d7c8bd9c23c3e321cf
mlyxshi <mlyxdev@gmail.com>
2023-06-13 18:28:00 +0800

    test random port

--- modules/network/default.nix
+++ modules/network/default.nix
@@ -31,6 +31,9 @@
 
         # Allow TFTP
         ${lib.optionalString (config.systemd.services ? tftpd) "udp dport 69 accept"}
+        
+        # Allow hysteria
+        ${lib.optionalString (config.systemd.services ? hysteria) "udp dport 8888 accept"}
       }
     }
   '' + lib.optionalString (config.systemd.services ? hysteria) ''
