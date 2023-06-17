7d8490ac2b33979eb38eb47f0822e47402db59fa
mlyxshi <mlyxdev@gmail.com>
2023-06-17 18:19:32 +0800

    update

--- modules/network/default.nix
+++ modules/network/default.nix
@@ -42,7 +42,7 @@
         type nat hook prerouting priority 0; policy accept;
 
         # https://hysteria.network/docs/port-hopping/
-        udp dport 60000-70000 redirect to :8888
+        udp dport 10000-60000 redirect to :8888
       }
     }
   '';
