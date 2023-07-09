f594bfcf28e1be51bc1d7324bffe0f47726d043e
mlyxshi <mlyxdev@gmail.com>
2023-07-09 23:49:42 +0800

    add snell

--- host/oracle/aarch64/jp2.nix
+++ host/oracle/aarch64/jp2.nix
@@ -1,17 +1,8 @@
 { self, pkgs, lib, config, ... }: {
   imports = [
     self.nixosModules.services.hysteria
+    self.nixosModules.services.snell
     self.nixosModules.services.tuic
     self.nixosModules.services.tftpd
-  ];
-
-  # snell-server hardcode /lib/ld-linux-aarch64.so.1
-  system.activationScripts.ldso = lib.stringAfter [ "usrbinenv" ] ''
-    mkdir -m 0755 -p /lib
-    ln -sfn ${pkgs.glibc}/lib/ld-linux-aarch64.so.1  /lib/ld-linux-aarch64.so.1 
-  '';
-
-  environment.systemPackages = with pkgs; [
-    snell
   ];
 }
