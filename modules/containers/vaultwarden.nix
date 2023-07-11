0e5c31353e94b2596d7dbf69b13edd406e2575c9
mlyxshi <mlyxdev@gmail.com>
2023-07-11 16:18:21 +0800

    vaultwarden 1.29.0

--- modules/containers/vaultwarden.nix
+++ modules/containers/vaultwarden.nix
@@ -7,29 +7,32 @@
 
   backup.vaultwarden = true;
 
+  # https://github.com/dani-garcia/vaultwarden/pull/3304
+  sops.secrets.vaultwarden.id = { };
+  sops.secrets.vaultwarden.key = { };
+  sops.templates.vaultwarden.content = ''
+    PUSH_ENABLED=true
+    PUSH_INSTALLATION_ID=${config.sops.placeholder.vaultwarden.id}
+    PUSH_INSTALLATION_KEY=${config.sops.placeholder.vaultwarden.key}
+  '';
+
   virtualisation.oci-containers.containers.vaultwarden = {
     image = "ghcr.io/dani-garcia/vaultwarden";
     environment = {
       SIGNUPS_ALLOWED = "false"; # Disable signups
       DOMAIN = "https://password.${config.networking.domain}"; # Yubikey FIDO2 WebAuthn
     };
+    environmentFiles = [
+      config.sops.templates.vaultwarden.path
+    ];
     volumes = [
       "/var/lib/vaultwarden:/data"
     ];
     extraOptions = lib.concatMap (x: [ "--label" x ]) [
       "io.containers.autoupdate=registry"
       "traefik.enable=true"
-
       "traefik.http.routers.vaultwarden.rule=Host(`password.${config.networking.domain}`)"
-      "traefik.http.routers.vaultwarden.service=vaultwarden"
       "traefik.http.routers.vaultwarden.entrypoints=websecure"
-
-      "traefik.http.routers.vaultwarden-websocket.rule=Host(`password.${config.networking.domain}`) && Path(`/notifications/hub`)"
-      "traefik.http.routers.vaultwarden-websocket.service=vaultwarden-websocket"
-      "traefik.http.routers.vaultwarden-websocket.entrypoints=websecure"
-
-      "traefik.http.services.vaultwarden.loadbalancer.server.port=80"
-      "traefik.http.services.vaultwarden-websocket.loadbalancer.server.port=3012"
     ] ++ [ "--no-healthcheck" ];
   };
 }
