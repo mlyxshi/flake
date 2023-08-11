be838edf62c28a3031b71b6c8d541cfc78d360c1
mlyxshi <mlyxdev@gmail.com>
2023-08-11 18:09:42 +0800

    test

--- host/oracle/aarch64/us1.nix
+++ host/oracle/aarch64/us1.nix
@@ -1,8 +1,5 @@
 { self, config, pkgs, lib, ... }: {
   imports = [
     self.nixosModules.services.hysteria
-    self.nixosModules.services.tuic
   ];
-
-  networking.nftables.enable = lib.mkForce false;
 }
