6c852bef9116e8bca88d156863a6a5a56931a9e8
mlyxshi <mlyxdev@gmail.com>
2023-11-19 17:23:05 +0900

    stable hydra

--- kexec/build.nix
+++ kexec/build.nix
@@ -7,7 +7,7 @@ let
   kexecScript = pkgs.writeTextDir "script/kexec-script" ''
     #!/usr/bin/env bash
     set -e   
-    echo "Downloading ${arch} kexec-musl-bin" && curl -LO http://hydra.mlyxshi.com/job/flake/kexec/${arch}/latest/download-by-type/file/kexec-bin && chmod +x ./kexec-bin
+    echo "Downloading ${arch} kexec-musl-bin" && curl -LO http://hydra.mlyxshi.com/job/flake/kexec/${arch}/latest/download-by-type/file/kexec && chmod +x ./kexec
     echo "Downloading ${arch} initrd" && curl -LO http://hydra.mlyxshi.com/job/flake/kexec/${arch}/latest/download-by-type/file/initrd
     echo "Downloading ${arch} kernel" && curl -LO http://hydra.mlyxshi.com/job/flake/kexec/${arch}/latest/download-by-type/file/kernel
 
@@ -28,8 +28,8 @@ let
     done
     
     echo "Wait ssh connection lost..., ssh root@ip and enjoy NixOS"
-    ./kexec-bin --kexec-syscall-auto --load ./kernel --initrd=./initrd  --append "init=/bin/init ${toString config.boot.kernelParams} ssh_host_key=$ssh_host_key ssh_authorized_key=$ssh_authorized_key $*"
-    ./kexec-bin -e
+    ./kexec --kexec-syscall-auto --load ./kernel --initrd=./initrd  --append "init=/bin/init ${toString config.boot.kernelParams} ssh_host_key=$ssh_host_key ssh_authorized_key=$ssh_authorized_key $*"
+    ./kexec -e
   '';
 
   ipxeScript = pkgs.writeTextDir "script/ipxe-script" ''
@@ -54,9 +54,9 @@ in
       cat > $out/nix-support/hydra-build-products <<EOF
       file initrd $out/initrd
       file kernel $out/${kernelTarget}
-      file kexec $out/script/kexec-script
+      file kexec-script $out/script/kexec-script
       file ipxe $out/script/ipxe-script
-      file kexec-bin $out/bin/kexec
+      file kexec $out/bin/kexec
       EOF
     '';
   };
