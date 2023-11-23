3ff555c28b1f14cd47c88dc149320f2c99d56e06
mlyxshi <mlyxdev@gmail.com>
2023-11-24 00:32:41 +0900

    hydra test

--- kexec/initrd/default.nix
+++ kexec/initrd/default.nix
@@ -12,7 +12,7 @@
     "/etc/nix/nix.conf".text = ''
       extra-experimental-features = nix-command flakes auto-allocate-uids
       auto-allocate-uids = true
-      substituters = https://cache.nixos.org https://oranc.li7g.com/ghcr.io/mlyxshi/nix
+      substituters = https://cache.nixos.org "https://cache.mlyxshi.com"
       trusted-public-keys = cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= cache:vXjiuWtSTOXj63zr+ZjMvXqvaYIK1atjyyEk+iuIqSg=
     '';
   };
