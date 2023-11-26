dccad860bb63226cc54e2327da32f8545f92ec68
mlyxshi <mlyxdev@gmail.com>
2023-11-27 02:17:14 +0900

    test repart

--- modules/network/default.nix
+++ modules/network/default.nix
@@ -10,34 +10,34 @@
 
 
   networking.firewall.enable = false; # Disable nixpkgs defined firewall
-  # networking.nftables.enable = true;
-  # networking.nftables.ruleset = ''
-  #   table inet FIREWALL {
-  #     chain INPUT {
-  #       # Drop all incoming traffic by default
-  #       type filter hook input priority 0; policy drop;
+  networking.nftables.enable = true;
+  networking.nftables.ruleset = ''
+    table inet FIREWALL {
+      chain INPUT {
+        # Drop all incoming traffic by default
+        type filter hook input priority 0; policy drop;
 
-  #       # Allow loopback traffic
-  #       iifname lo accept
+        # Allow loopback traffic
+        iifname lo accept
 
-  #       # Allow ICMP
-  #       ip protocol icmp accept
+        # Allow ICMP
+        ip protocol icmp accept
 
-  #       # Accept traffic originated from us
-  #       ct state {established, related} accept
+        # Accept traffic originated from us
+        ct state {established, related} accept
 
-  #       # Only Allow SSH and Traefik
-  #       tcp dport { 22, 80, 443 } accept
+        # Only Allow SSH and Traefik
+        tcp dport { 22, 80, 443 } accept
 
-  #       # Allow TFTP
-  #       ${lib.optionalString (config.systemd.services ? tftpd) "udp dport 69 accept"}
+        # Allow TFTP
+        ${lib.optionalString (config.systemd.services ? tftpd) "udp dport 69 accept"}
         
-  #       # Allow tuic
-  #       # ${lib.optionalString (config.systemd.services ? tuic) "udp dport 6666 accept"}
+        # Allow tuic
+        # ${lib.optionalString (config.systemd.services ? tuic) "udp dport 6666 accept"}
 
-  #       # Allow hysteria
-  #       ${lib.optionalString (config.systemd.services ? hysteria) "udp dport 443 accept"}
-  #     }
-  #   }
-  # '';
+        # Allow hysteria
+        ${lib.optionalString (config.systemd.services ? hysteria) "udp dport 443 accept"}
+      }
+    }
+  '';
 }
