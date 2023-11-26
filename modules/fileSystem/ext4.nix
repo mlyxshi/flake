c67c5b14f4d02f34cf9c11b44d00fd0a59d476fb
mlyxshi <mlyxdev@gmail.com>
2023-11-27 01:18:41 +0900

    test repart

--- modules/fileSystem/ext4.nix
+++ modules/fileSystem/ext4.nix
@@ -1,11 +1,11 @@
 {
   fileSystems."/boot" = {
-    device = "/dev/disk/by-label/BOOT";
+    device = "/dev/disk/by-partlabel/BOOT";
     fsType = "vfat";
   };
 
   fileSystems."/" = {
-    device = "/dev/disk/by-label/NIXOS";
+    device = "/dev/disk/by-partlabel/NIXOS";
     fsType = "ext4";
   };
 
