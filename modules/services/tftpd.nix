347320911d7616d850ac51778d2074d150cbacb4
mlyxshi <mlyxdev@gmail.com>
2023-11-27 02:44:23 +0900

    test repart

--- modules/services/tftpd.nix
+++ modules/services/tftpd.nix
@@ -1,28 +1,30 @@
 { config, pkgs, lib, ... }: {
-  users = {
-    users.tftpd = {
-      group = "tftpd";
-      isSystemUser = true;
-    };
-    groups.tftpd = { };
-  };
+  # users = {
+  #   users.tftpd = {
+  #     group = "tftpd";
+  #     isSystemUser = true;
+  #   };
+  #   groups.tftpd = { };
+  # };
 
-  # https://manpages.debian.org/testing/tftpd-hpa/tftpd.8.en.html
-  systemd.services.tftpd = {
-    after = [ "network-online.target" ];
-    serviceConfig.ExecStart = "${pkgs.tftp-hpa}/bin/in.tftpd --user tftpd --verbose --listen --foreground --secure  %S/tftpd";
-    postStart = ''
-      [ -e "netboot.xyz.efi" ] || ${pkgs.wget}/bin/wget  https://boot.netboot.xyz/ipxe/netboot.xyz.efi
-      [ -e "netboot.xyz-arm64.efi" ] || ${pkgs.wget}/bin/wget  https://boot.netboot.xyz/ipxe/netboot.xyz-arm64.efi
-      chmod 444 netboot.xyz.efi
-      chmod 444 netboot.xyz-arm64.efi
-    '';
-    serviceConfig.User = "tftpd";
-    serviceConfig.AmbientCapabilities = "CAP_NET_BIND_SERVICE";
-    serviceConfig.WorkingDirectory = "%S/tftpd";
-    serviceConfig.StateDirectory = "tftpd";
-    wantedBy = [ "multi-user.target" ];
-  };
+  # # https://manpages.debian.org/testing/tftpd-hpa/tftpd.8.en.html
+  # systemd.services.tftpd = {
+  #   after = [ "network-online.target" ];
+  #   serviceConfig.ExecStart = "${pkgs.tftp-hpa}/bin/in.tftpd --user tftpd --verbose --listen --foreground --secure  %S/tftpd";
+  #   postStart = ''
+  #     [ -e "netboot.xyz.efi" ] || ${pkgs.wget}/bin/wget  https://boot.netboot.xyz/ipxe/netboot.xyz.efi
+  #     [ -e "netboot.xyz-arm64.efi" ] || ${pkgs.wget}/bin/wget  https://boot.netboot.xyz/ipxe/netboot.xyz-arm64.efi
+  #     chmod 444 netboot.xyz.efi
+  #     chmod 444 netboot.xyz-arm64.efi
+  #   '';
+  #   serviceConfig.User = "tftpd";
+  #   serviceConfig.AmbientCapabilities = "CAP_NET_BIND_SERVICE";
+  #   serviceConfig.WorkingDirectory = "%S/tftpd";
+  #   serviceConfig.StateDirectory = "tftpd";
+  #   wantedBy = [ "multi-user.target" ];
+  # };
+  services.atftpd.enable = true;
+  services.atftpd.root = "/var/lib/netboot";
 }
 
 # UEFI Shell
