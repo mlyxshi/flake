e9d7408e876c09a151ba28e56e93fa4b4b3c1252
mlyxshi <mlyxdev@gmail.com>
2023-11-27 17:18:51 +0900

    test repart

--- modules/repart/default.nix
+++ modules/repart/default.nix
@@ -10,29 +10,20 @@ in
 
   #fileSystems."/".autoResize = true;
 
-  boot.initrd.systemd.repart.enable = true;
-  boot.initrd.systemd.repart.device = "/dev/sda";
-
   image.repart = {
     name = config.networking.hostName;
     partitions = {
       "BOOT" = {
         contents = {
-          "/EFI/BOOT/BOOT${lib.toUpper efiArch}.EFI".source =
-            "${pkgs.systemd}/lib/systemd/boot/efi/systemd-boot${efiArch}.efi";
-
+          "/EFI/BOOT/BOOT${lib.toUpper efiArch}.EFI".source = "${pkgs.systemd}/lib/systemd/boot/efi/systemd-boot${efiArch}.efi";
+          "/EFI/nixos/kernel.efi".source = "${config.boot.kernelPackages.kernel}/${config.system.boot.loader.kernelFile}";
+          "/EFI/nixos/initrd.efi".source = "${config.system.build.initialRamdisk}/${config.system.boot.loader.initrdFile}";
           "/loader/entries/nixos.conf".source = pkgs.writeText "nixos.conf" ''
             title NixOS
             linux /EFI/nixos/kernel.efi
             initrd /EFI/nixos/initrd.efi
             options init=${config.system.build.toplevel}/init ${toString config.boot.kernelParams}
           '';
-
-          "/EFI/nixos/kernel.efi".source =
-            "${config.boot.kernelPackages.kernel}/${config.system.boot.loader.kernelFile}";
-
-          "/EFI/nixos/initrd.efi".source =
-            "${config.system.build.initialRamdisk}/${config.system.boot.loader.initrdFile}";
         };
         repartConfig = {
           Type = "esp";
