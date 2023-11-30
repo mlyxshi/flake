0d143f77aac6da2c1fadadd167b3715a7fd72ee3
mlyxshi <mlyxdev@gmail.com>
2023-11-30 18:37:34 +0900

    utm

--- flake.nix
+++ flake.nix
@@ -29,7 +29,7 @@
       darwinConfigurations.M1 = import ./host/M1 { inherit self nixpkgs darwin; };
       nixosConfigurations = {
         hx90 = import ./host/hx90 { inherit self nixpkgs sops-nix nix-index-database; };
-        utm = import ./host/utm { inherit self nixpkgs; };
+        utm = import ./host/utm { inherit self nixpkgs sops-nix; };
         qemu-test-x64 = import ./host/oracle/mkTest.nix { inherit self nixpkgs sops-nix; };
 
         kexec-x86_64 = import ./kexec/mkKexec.nix { arch = "x86_64"; inherit self nixpkgs; };
