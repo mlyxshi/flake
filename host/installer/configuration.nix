28e9b19d85a36f63ea89e9f74a73de2c95e1a615
mlyxshi <mlyxdev@gmail.com>
2023-12-01 00:26:25 +0900

    utm

--- host/installer/configuration.nix
+++ host/installer/configuration.nix
@@ -1,6 +1,15 @@
 { modulesPath, lib, pkgs, ... }: {
   imports = [
-    (modulesPath + "/installer/cd-dvd/installation-cd-minimal-new-kernel-no-zfs.nix")
+    (modulesPath + "/installer/cd-dvd/installation-cd-minimal.nix")
+  ];
+
+  # https://github.com/NixOS/nixpkgs/blob/84d2fa520e16fd45d99a69d6a0bb25d9e096327f/nixos/modules/installer/cd-dvd/installation-cd-minimal-new-kernel-no-zfs.nix#L6
+  nixpkgs.overlays = [
+    (final: super: {
+      zfs = super.zfs.overrideAttrs (_: {
+        meta.platforms = [ ];
+      });
+    })
   ];
 
   services.getty.autologinUser = lib.mkForce "root";
