038436d56f141e6f6235b4926944e8d9ad1b09f3
mlyxshi <mlyxdev@gmail.com>
2023-11-30 23:18:10 +0900

    utm

--- flake.nix
+++ flake.nix
@@ -33,7 +33,7 @@
 
         qemu-test-x86-64 = import ./host/oracle/mkTest.nix { arch = "x86_64"; inherit self nixpkgs sops-nix; };
         qemu-test-aarch64 = import ./host/oracle/mkTest.nix { arch = "aarch64"; inherit self nixpkgs sops-nix; };
-
+        
         kexec-x86_64 = import ./kexec/mkKexec.nix { arch = "x86_64"; inherit self nixpkgs; };
         kexec-aarch64 = import ./kexec/mkKexec.nix { arch = "aarch64"; inherit self nixpkgs; };
       }
