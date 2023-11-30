8bf3817b01d24a0c1b52d3f74086ba2470fb633c
mlyxshi <mlyxdev@gmail.com>
2023-11-30 18:42:10 +0900

    utm

--- host/utm/default.nix
+++ host/utm/default.nix
@@ -8,6 +8,7 @@ nixpkgs.lib.nixosSystem {
     self.nixosModules.fileSystem.ext4
     self.nixosModules.settings.developerMode
     self.nixosModules.services.ssh-config
+    ./hardware.nix
     {
       nixpkgs.overlays = [ self.overlays.default ];
       nixpkgs.hostPlatform = "aarch64-linux";
