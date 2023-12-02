8430a05c8eba5b8095a7324306b3cd073d2f1ca8
mlyxshi <mlyxdev@gmail.com>
2023-12-02 12:18:36 +0900

    test

--- kexec/initrd/default.nix
+++ kexec/initrd/default.nix
@@ -1,6 +1,6 @@
 { config, pkgs, lib, ... }: {
   imports = [
-    ./install.nix
+    #./install.nix
     ./network.nix
     ./kernelModules.nix
   ];
@@ -62,20 +62,20 @@
   # move everything in / to /sysroot and switch-root into it. 
   # This runs a few things twice and wastes some memory
   # but is necessary for nix --store flag as pivot_root does not work on rootfs.
-  boot.initrd.systemd.services.remount-root = {
-    before = [ "initrd-fs.target" ];
-    serviceConfig.Type = "oneshot";
-    script = ''
-      ls -l /
-      root_fs_type="$(mount|awk '$3 == "/" { print $1 }')"
-      if [ "$root_fs_type" != "tmpfs" ]; then
-        cp -R /init /bin /etc /lib /nix /root /sbin /var  /sysroot
-        mkdir -p /sysroot/tmp
-        systemctl --no-block switch-root /sysroot /bin/init
-      fi
-    '';
-    requiredBy = [ "initrd-fs.target" ];
-  };
+  # boot.initrd.systemd.services.remount-root = {
+  #   before = [ "initrd-fs.target" ];
+  #   serviceConfig.Type = "oneshot";
+  #   script = ''
+  #     ls -l /
+  #     root_fs_type="$(mount|awk '$3 == "/" { print $1 }')"
+  #     if [ "$root_fs_type" != "tmpfs" ]; then
+  #       cp -R /init /bin /etc /lib /nix /root /sbin /var  /sysroot
+  #       mkdir -p /sysroot/tmp
+  #       systemctl --no-block switch-root /sysroot /bin/init
+  #     fi
+  #   '';
+  #   requiredBy = [ "initrd-fs.target" ];
+  # };
 
 
   # Disable default services in Nixpkgs
