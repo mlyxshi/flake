c973f920713d3097126b4e5a68e23b2b45e5edab
mlyxshi <mlyxdev@gmail.com>
2023-12-02 15:04:36 +0900

    test

--- host/utm/misc.nix
+++ host/utm/misc.nix
@@ -6,14 +6,25 @@ let
     AGE_KEY_URL=$3
     BARK_KEY=$4
 
+    [[ -e "/persist/flake/flake.nix" ]] || git clone --depth=1  git@github.com:mlyxshi/flake /persist/flake
+      
+    cd /persist/flake
+    git pull 
+
     outPath=$(nix build --no-link --print-out-paths .#nixosConfigurations.$HOST.config.system.build.toplevel)
     
-    ssh -o StrictHostKeyChecking=no root@$IP
-    while test $? -gt 0
+    # ssh -o StrictHostKeyChecking=no root@$IP
+    # while test $? -gt 0
+    # do
+    #   sleep 5 
+    #   echo "Wait kexec loading and ssh server, Trying again..."
+    #   ssh -o StrictHostKeyChecking=no root@$IP
+    # done
+
+    until ssh -o StrictHostKeyChecking=no root@$IP 2> /dev/null
     do
-      sleep 5 
       echo "Wait kexec loading and ssh server, Trying again..."
-      ssh -o StrictHostKeyChecking=no root@$IP
+      sleep 5
     done
 
     ssh -o StrictHostKeyChecking=no root@$IP parted --script /dev/sda mklabel gpt mkpart "BOOT" fat32  1MiB  512MiB mkpart "NIXOS" ext4 512MiB 100% set 1 esp on
