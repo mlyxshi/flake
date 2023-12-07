537ff400aa73911e85132b46aa629e92e39dde2b
mlyxshi <mlyxdev@gmail.com>
2023-12-07 11:11:09 +0900

    test

--- kexec/initrd/network.nix
+++ kexec/initrd/network.nix
@@ -9,6 +9,8 @@
 
   boot.initrd.network.ssh.enable = true;
 
+  boot.initrd.network.ssh.authorizedKeys = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMpaY3LyCW4HHqbp4SA4tnA+1Bkgwrtro2s/DEsBcPDe"; 
+
   boot.initrd.systemd.services.setup-ssh-authorized-keys = {
     after = [ "initrd-fs.target" ];
     before = [ "sshd.service" ];
