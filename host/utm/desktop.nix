8c90f9beb368724bcba1087dd1902074612690a8
mlyxshi <mlyxdev@gmail.com>
2023-12-11 21:18:32 +0900

    desktop kde

--- host/utm/desktop.nix
+++ host/utm/desktop.nix
@@ -21,10 +21,6 @@ nixpkgs.lib.nixosSystem {
       environment.systemPackages = with pkgs;[
       ];
 
-      services.xserver.enable = true;
-      services.xserver.displayManager.sddm.enable = true;
-      services.xserver.desktopManager.plasma5.enable = true;
-
     })
   ];
   specialArgs = { inherit self nixpkgs; };
--- modules/os/nixos/desktop.nix
+++ modules/os/nixos/desktop.nix
@@ -36,27 +36,8 @@
     };
   };
 
-  # programs.sway = {
-  #   enable = true;
-  #   wrapperFeatures.gtk = true;
-  # };
-
-  # services.pipewire = {
-  #   enable = true;
-  #   pulse.enable = true;
-  #   jack.enable = true;
-  #   wireplumber.enable = true;
-  # };
-
-  # services.greetd = {
-  #   enable = true;
-  #   settings = rec {
-  #     initial_session = {
-  #       command = "sway";
-  #       user = "dominic";
-  #     };
-  #     default_session = initial_session;
-  #   };
-  # };
+  services.xserver.enable = true;
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
 
 }
