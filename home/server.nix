97e806356bf81949bab5ecabc8405317346daa87
mlyxshi <mlyxdev@gmail.com>
2023-12-24 18:16:34 +0900

    hm

--- home/server.nix
+++ home/server.nix
@@ -2,6 +2,6 @@
   imports = [ ./common.nix ];
 
   home.file.".config/helix".source = ../config/helix;
-  home.file.".config/git".source = .config/git;
-  home.file.".config/joshuto".source = .config/joshuto;
+  home.file.".config/git".source = ../config/git;
+  home.file.".config/joshuto".source = ../config/joshuto;
 }
