79a22fa38f9f44212f699d35f109afe5ca782f85
mlyxshi <mlyxdev@gmail.com>
2023-12-24 17:36:48 +0900

    asahi

--- home/asahi.nix
+++ home/asahi.nix
@@ -1,4 +1,8 @@
 { pkgs, lib, config, ... }: {
+  imports =[
+    ./linux.nix
+  ];
+  
   programs.firefox.package = lib.mkForce pkgs.firefox;
-  programs.firefox.policies = import ./firefox/policy.nix;
+  programs.firefox.policies = (import ./firefox/policy.nix).policies;
 }
