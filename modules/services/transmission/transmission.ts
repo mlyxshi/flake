3ca381cb198fbcf5985ad52123b4f320c88535b1
mlyxshi <mlyxdev@gmail.com>
2023-12-31 13:40:54 +0900

    update flake

--- modules/services/transmission/transmission.ts
+++ modules/services/transmission/transmission.ts
@@ -18,10 +18,6 @@ console.log("TR_TORRENT_LABELS:", TR_TORRENT_LABELS);
 console.log("TR_TORRENT_DIR:", TR_TORRENT_DIR);
 console.log("FILE_INFO:", FILE_INFO)
 
-function isSingleVideo(str) {
-    const videoSuffix = ['.mp4', '.avi', '.mkv', '.mov', '.flv', '.wmv', '.webm'];
-    return videoSuffix.some(suffix => str.endsWith(suffix));
-}
 
 if (TR_TORRENT_LABELS == "rss") {
     Deno.run({ cmd: ["transmission-remote", "--auth", `${ADMIN}:${PASSWORD}`, "--torrent", TR_TORRENT_ID, "--remove"], });
@@ -35,25 +31,7 @@ if (TR_TORRENT_LABELS == "rss") {
             body: TR_TORRENT_NAME,
         }),
     },);
-} else if (FILE_INFO.isFile && isSingleVideo(TR_TORRENT_NAME)) {
-    const MEDIA_URL = encodeURIComponent(`http://transmission-index.mlyxshi.com/${TR_TORRENT_NAME}`);
-    const IOS_SHORTCUTS_URL = encodeURIComponent(`shortcuts://run-shortcut?name=transmission-delete&input=text&text=${TR_TORRENT_ID}`)
-    const INFUSE_URL_SCHEME = `infuse://x-callback-url/play?url=${MEDIA_URL}&x-success=${IOS_SHORTCUTS_URL}`;
-
-    fetch(`https://api.day.app/push`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json", },
-        body: JSON.stringify({
-            device_key: BARK_KEY,
-            title: "Infuse",
-            icon: "https://static.firecore.com/images/infuse/infuse-icon_2x.png",
-            body: TR_TORRENT_NAME,
-            url: INFUSE_URL_SCHEME,
-            copy: INFUSE_URL_SCHEME,
-        }),
-    });
-
-} 
+}
 
 // else {
 //     let cmd = "";
