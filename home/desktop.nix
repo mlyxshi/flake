63886fb206898cc6a6df89f68341816fe3bd1986
mlyxshi <mlyxdev@gmail.com>
2024-01-03 18:32:46 +0900

    xremap homemanager

--- home/desktop.nix
+++ home/desktop.nix
@@ -1,7 +1,55 @@
-{ pkgs, lib, config, ... }: {
+{ pkgs, lib, config, xremap, ... }: {
 
   imports = [
-    ./default.nix
     ./firefox
+    xremap.homeManagerModules.default
   ];
+
+  services.xremap = {
+    withKDE = true;
+    config = {
+      # modmap = [
+      #   {
+      #     name = "Global";
+      #     remap = { 
+      #        = "";
+      #     }; # globally remap
+      #   }
+      # ];
+      # other xremap settings go here
+
+      keymap = [
+        {
+          name = "edit";
+          remap = {
+            SUPER-z = "CTRL-z";
+            SUPER-x = "CTRL-x";
+            SUPER-c = "CTRL-c";
+            SUPER-v = "CTRL-v";
+            SUPER-a = "CTRL-a";
+            SUPER-s = "CTRL-s";
+            SUPER-t = "CTRL-t";
+            SUPER-f = "CTRL-f";
+          };
+          application.only = [
+            "firefox"
+            "code"
+          ];
+        }
+
+        {
+          name = "konsole";
+          remap = {
+            SUPER-c = "CTRL-SHIFT-c";
+            SUPER-v = "CTRL-SHIFT-v";
+            SUPER-t = "CTRL-SHIFT-t";
+            SUPER-f = "CTRL-SHIFT-f";
+          };
+          application.only = [
+            "konsole"
+          ];
+        }
+      ];
+    };
+  };
 }
--- host/utm/default.nix
+++ host/utm/default.nix
@@ -20,61 +20,15 @@ nixpkgs.lib.nixosSystem {
       users.groups.uinput.members = [ "dominic" ];
       users.groups.input.members = [ "dominic" ];
 
-      home-manager.users.root = import ../../home;
-      home-manager.users.dominic = {
-        imports = [
-          ../../home/desktop.nix
-          xremap.homeManagerModules.default
-        ];
-        services.xremap = {
-          withKDE = true;
-          config = {
-            # modmap = [
-            #   {
-            #     name = "Global";
-            #     remap = { 
-            #        = "";
-            #     }; # globally remap CapsLock to Esc
-            #   }
-            # ];
-            # other xremap settings go here
+      home-manager.sharedModules = [
+        ../../home
+      ];
 
-            keymap = [
-              {
-                name = "edit";
-                remap = {
-                  SUPER-z = "CTRL-z";
-                  SUPER-x = "CTRL-x";
-                  SUPER-c = "CTRL-c";
-                  SUPER-v = "CTRL-v";
-                  SUPER-a = "CTRL-a";
-                  SUPER-s = "CTRL-s";
-                  SUPER-t = "CTRL-t";
-                  SUPER-f = "CTRL-f";
-                };
-                application.only = [
-                  "firefox"
-                  "code"
-                ];
-              }
-
-              {
-                name = "konsole";
-                remap = {
-                  SUPER-c = "CTRL-SHIFT-c";
-                  SUPER-v = "CTRL-SHIFT-v";
-                  SUPER-t = "CTRL-SHIFT-t";
-                  SUPER-f = "CTRL-SHIFT-f";
-                };
-                application.only = [
-                  "konsole"
-                ];
-              }
-            ];
-          };
-        };
-
+      home-manager.extraSpecialArgs = {
+        inherit xremap;
       };
+
+      home-manager.users.dominic = import ../../home/desktop.nix;
       home-manager.useGlobalPkgs = true;
       home-manager.useUserPackages = true;
       home-manager.verbose = true;
