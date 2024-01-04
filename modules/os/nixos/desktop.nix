3269eda3aa65f73687c25fbe8dc0728a66a3ba96
mlyxshi <mlyxdev@gmail.com>
2024-01-04 16:48:01 +0900

    .

--- modules/os/nixos/desktop.nix
+++ modules/os/nixos/desktop.nix
@@ -43,4 +43,6 @@
   services.xserver.enable = true;
   services.xserver.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
+
+  networking.networkmanager.enable = true;
 }
