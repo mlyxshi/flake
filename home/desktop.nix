348a48a112c73ec7919f54f1e3c9cc1dbc8f75ff
mlyxshi <mlyxdev@gmail.com>
2024-01-04 16:58:40 +0900

    .

--- home/desktop.nix
+++ home/desktop.nix
@@ -1,63 +1,9 @@
-{ pkgs, lib, config, xremap, ... }: {
+{ pkgs, lib, config, ... }: {
 
   imports = [
     ./.
     ./firefox
-    xremap.homeManagerModules.default
+    ./xremap.nix
   ];
 
-  services.xremap = {
-    withKDE = true;
-    config = {
-      # globally remap
-      # modmap = [
-      #   {
-      #     name = "Global";
-      #     remap = { 
-      #        = "";
-      #     };
-      #   }
-      # ];
-
-      keymap = [
-        {
-          name = "edit";
-          remap = {
-            SUPER-z = "CTRL-z";
-            SUPER-x = "CTRL-x";
-            SUPER-c = "CTRL-c";
-            SUPER-v = "CTRL-v";
-            SUPER-a = "CTRL-a";
-            SUPER-s = "CTRL-s";
-            SUPER-t = "CTRL-t";
-            SUPER-f = "CTRL-f";
-          };
-          application.only = [
-            "firefox"
-            "code"
-          ];
-        }
-
-        {
-          name = "konsole";
-          remap = {
-            SUPER-c = "CTRL-SHIFT-c";
-            SUPER-v = "CTRL-SHIFT-v";
-            SUPER-t = "CTRL-SHIFT-t";
-            SUPER-f = "CTRL-SHIFT-f";
-          };
-          application.only = [
-            "konsole"
-          ];
-        }
-
-        {
-          name = "appswitcher";
-          remap = {
-            SUPER-TAB = "ALT-TAB";
-          };
-        }
-      ];
-    };
-  };
 }
