13c323742ff459c249f687d0657821d721ea3a59
mlyxshi <mlyxdev@gmail.com>
2024-01-05 14:57:28 +0900

    flake update

--- asahi.sh
+++ asahi.sh
@@ -11,8 +11,7 @@ sudo ln -sf /home/dominic/flake/config/etc/firefox/policies/policies.json /etc/f
 # sudo usermod -aG input dominic
 
 git clone https://github.com/RedBearAK/toshy ~/toshy
-cd ~/toshy
-./setup_toshy.py install
+~/toshy/setup_toshy.py install
 
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
