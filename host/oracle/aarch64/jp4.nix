9015565faa7381892a79f989b57ac0a42e6cd459
mlyxshi <mlyxdev@gmail.com>
2024-01-10 23:06:39 +0900

    flake update

--- host/oracle/aarch64/jp4.nix
+++ host/oracle/aarch64/jp4.nix
@@ -4,6 +4,7 @@
 
     self.nixosModules.containers.jellyfin
     self.nixosModules.containers.navidrome
+    self.nixosModules.containers.change-detection
   ];
 
   networking.nftables.enable = lib.mkForce false;
