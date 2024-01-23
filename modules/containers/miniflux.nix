14900f18828a6b31bd2fba2fd566dd677aef5ba9
mlyxshi <mlyxdev@gmail.com>
2024-01-23 14:53:21 +0900

    test miniflux

--- modules/containers/miniflux.nix
+++ modules/containers/miniflux.nix
@@ -54,4 +54,44 @@
   };
 
 
+  #################################
+  # silent miniflux instance for unimportant feeds
+
+  # virtualisation.oci-containers.containers.miniflux-silent = {
+  #   image = "ghcr.io/miniflux/miniflux";
+  #   dependsOn = [
+  #     "miniflux-silent-postgres"
+  #   ];
+  #   environment = {
+  #     CREATE_ADMIN = "1";
+  #     RUN_MIGRATIONS = "1";
+  #     POLLING_FREQUENCY = "10";
+  #     POLLING_PARSING_ERROR_LIMIT = "0";
+  #     METRICS_COLLECTOR = "1";
+  #     METRICS_ALLOWED_NETWORKS = "0.0.0.0/0";
+  #     DATABASE_URL = "postgres://postgres:postgres@miniflux-silent-postgres/miniflux?sslmode=disable";
+  #     BASE_URL = "https://test-silent.${config.networking.domain}";
+  #   };
+  #   environmentFiles = [
+  #     config.sops.templates.miniflux-admin-credentials.path
+  #   ];
+  #   extraOptions = lib.concatMap (x: [ "--label" x ]) [
+  #     "io.containers.autoupdate=registry"
+  #     "traefik.enable=true"
+  #     "traefik.http.routers.miniflux.rule=Host(`test-silent.${config.networking.domain}`)"
+  #     "traefik.http.routers.miniflux.entrypoints=websecure"
+  #   ];
+  # };
+
+  virtualisation.oci-containers.containers.miniflux-silent-postgres = {
+    image = "docker.io/library/postgres:15";
+    environment = {
+      POSTGRES_USER = "postgres";
+      POSTGRES_PASSWORD = "postgres";
+      POSTGRES_DB = "miniflux";
+    };
+    volumes = [
+      "/var/lib/miniflux-silent-postgres:/var/lib/postgresql/data"
+    ];
+  };
 }
