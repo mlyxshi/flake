929c8243ded1c73f29ecbcaa202b332fce9c4b97
mlyxshi <mlyxdev@gmail.com>
2024-01-27 17:18:43 +0900

    clean

--- asahi.sh
+++ asahi.sh
@@ -1,16 +1,16 @@
 nix run --extra-substituters https://cache.mlyxshi.com --extra-trusted-public-keys cache:vXjiuWtSTOXj63zr+ZjMvXqvaYIK1atjyyEk+iuIqSg=  ~/flake#homeConfigurations.deck.activationPackage
 
 sudo mkdir -p /etc/firefox/policies
-nix eval --json --file ./home/firefox/policy.nix  | sudo tee /etc/firefox/policies/policies.json
+nix eval --json --file /home/dominic/flake/home/firefox/policy.nix  | sudo tee /etc/firefox/policies/policies.json
 
 echo 'KERNEL=="uinput", GROUP="input", TAG+="uaccess"' | sudo tee /etc/udev/rules.d/input.rules
 sudo usermod -aG input dominic
 
 dnf check-update
-sudo dnf install code neochat mpv
+sudo dnf install mpv
 
-sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-flatpak install org.telegram.desktop
+# sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+# flatpak install org.telegram.desktop
 
 # kde connect
 sudo systemctl disable firewalld.service --now
--- deck.sh
+++ deck.sh
@@ -1,7 +1,7 @@
 nix run --extra-substituters https://cache.mlyxshi.com --extra-trusted-public-keys cache:vXjiuWtSTOXj63zr+ZjMvXqvaYIK1atjyyEk+iuIqSg=  ~/flake#homeConfigurations.deck.activationPackage
 
 sudo mkdir -p /etc/firefox/policies
-nix eval --json --file ./home/firefox/policy.nix  | sudo tee /etc/firefox/policies/policies.json
+nix eval --json --file /home/deck/flake/home/firefox/policy.nix  | sudo tee /etc/firefox/policies/policies.json
 
 echo 'KERNEL=="uinput", GROUP="input", TAG+="uaccess"' | sudo tee /etc/udev/rules.d/input.rules
 sudo usermod -aG input deck
