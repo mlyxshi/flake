0c6b6878d2e445bcb93ffd6545079c3458cbf9d2
mlyxshi <mlyxdev@gmail.com>
2024-01-30 19:36:03 +0900

    clean

--- host/oracle/aarch64/sw.nix
+++ host/oracle/aarch64/sw.nix
@@ -1,6 +1,8 @@
 { self, config, pkgs, lib, ... }: {
   # portal enabled firewall
+  networking.nftables.enable = lib.mkForce false;
+  
   imports = [
-
+    self.nixosModules.containers.miniflux
   ];
 }
--- host/oracle/aarch64/us2.nix
+++ host/oracle/aarch64/us2.nix
@@ -1,6 +1,3 @@
 { self, config, pkgs, lib, ... }: {
-  imports = [
-    self.nixosModules.containers.miniflux
-  ];
-  networking.nftables.enable = lib.mkForce false;
+
 }
