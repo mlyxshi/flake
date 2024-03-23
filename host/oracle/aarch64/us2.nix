06d567e57f2b97f17816c085994f12d50f1176e5
mlyxshi <mlyxdev@gmail.com>
2024-03-23 21:50:18 +0900

    test

--- host/oracle/aarch64/us2.nix
+++ host/oracle/aarch64/us2.nix
@@ -17,63 +17,90 @@ in
   networking.nftables.enable = lib.mkForce false;
 
 
-  users = {
-    users.transmission = {
-      group = "transmission";
-      isNormalUser = true;
-    };
-    groups.transmission = { };
-  };
-
-  vpnnamespaces.wg = {
+vpnnamespaces.wg = {
     enable = true;
     accessibleFrom = [
       "192.168.0.0/24"
     ];
-    wireguardConfigFile = "/etc/secret/wg0.conf";
+    wireguardConfigFile = "/etc/secrets/wg0.conf";
     portMappings = [
-      # { from = 22; to = 22; } # tcp is default
-      # { from = 22; to = 22; protocol = "tcp"; }
-      # { from = 8080; to = 80; protocol = "udp"; }
-      # { from = 443; to = 443; protocol = "both"; }
+      { from = 22; to = 22; } # tcp is default
+      { from = 22; to = 22; protocol = "tcp"; }
+      { from = 8080; to = 80; protocol = "udp"; }
+      { from = 443; to = 443; protocol = "both"; }
     ];
   };
 
   # Enable and specify VPN namespace to confine service in.
+  systemd.services.transmission.vpnconfinement = {
+    enable = true;
+    vpnnamespace = "wg";
+  };
 
-  # services.transmission = {
-  #   enable = true;
-  #   settings = {
-  #     "rpc-bind-address" = "192.168.15.1"; # Bind RPC/WebUI to bridge address
+  services.transmission = {
+    enable = true;
+    settings = {
+      "rpc-bind-address" = "192.168.15.1"; # Bind RPC/WebUI to bridge address
+    };
+  };
+
+  # users = {
+  #   users.transmission = {
+  #     group = "transmission";
+  #     isNormalUser = true;
   #   };
+  #   groups.transmission = { };
   # };
 
-  systemd.services.transmission-init = {
-    unitConfig.ConditionPathExists = "!%S/transmission/settings.json";
-    script = ''
-      cat ${settings} > settings.json
-    '';
-    serviceConfig.User = "transmission";
-    serviceConfig.Type = "oneshot";
-    serviceConfig.StateDirectory = "transmission";
-    serviceConfig.WorkingDirectory = "%S/transmission";
-    wantedBy = [ "multi-user.target" ];
-  };
+  # vpnnamespaces.wg = {
+  #   enable = true;
+  #   accessibleFrom = [
+  #     "192.168.0.0/24"
+  #   ];
+  #   wireguardConfigFile = "/etc/secret/wg0.conf";
+  #   portMappings = [
+  #     # { from = 22; to = 22; } # tcp is default
+  #     # { from = 22; to = 22; protocol = "tcp"; }
+  #     # { from = 8080; to = 80; protocol = "udp"; }
+  #     # { from = 443; to = 443; protocol = "both"; }
+  #   ];
+  # };
 
-  systemd.services.transmission = {
-    after = [ "transmission-init.service" "network-online.target" ];
-    wants = [ "network-online.target" ];
-    environment = {
-      TRANSMISSION_HOME = "%S/transmission";
-      TRANSMISSION_WEB_HOME = "${pkgs.transmission}/public_html";
-    };
-    serviceConfig.User = "transmission";
-    serviceConfig.EnvironmentFile = [ "/etc/secret/transmission" ];
-    serviceConfig.ExecStart =
-      "${pkgs.transmission}/bin/transmission-daemon --foreground --username $ADMIN --password $PASSWORD";
-    serviceConfig.WorkingDirectory = "%S/transmission";
-    wantedBy = [ "multi-user.target" ];
-  };
+  # # Enable and specify VPN namespace to confine service in.
+
+  # # services.transmission = {
+  # #   enable = true;
+  # #   settings = {
+  # #     "rpc-bind-address" = "192.168.15.1"; # Bind RPC/WebUI to bridge address
+  # #   };
+  # # };
+
+  # systemd.services.transmission-init = {
+  #   unitConfig.ConditionPathExists = "!%S/transmission/settings.json";
+  #   script = ''
+  #     cat ${settings} > settings.json
+  #   '';
+  #   serviceConfig.User = "transmission";
+  #   serviceConfig.Type = "oneshot";
+  #   serviceConfig.StateDirectory = "transmission";
+  #   serviceConfig.WorkingDirectory = "%S/transmission";
+  #   wantedBy = [ "multi-user.target" ];
+  # };
+
+  # systemd.services.transmission = {
+  #   after = [ "transmission-init.service" "network-online.target" ];
+  #   wants = [ "network-online.target" ];
+  #   environment = {
+  #     TRANSMISSION_HOME = "%S/transmission";
+  #     TRANSMISSION_WEB_HOME = "${pkgs.transmission}/public_html";
+  #   };
+  #   serviceConfig.User = "transmission";
+  #   serviceConfig.EnvironmentFile = [ "/etc/secret/transmission" ];
+  #   serviceConfig.ExecStart =
+  #     "${pkgs.transmission}/bin/transmission-daemon --foreground --username $ADMIN --password $PASSWORD";
+  #   serviceConfig.WorkingDirectory = "%S/transmission";
+  #   wantedBy = [ "multi-user.target" ];
+  # };
 
 
 
