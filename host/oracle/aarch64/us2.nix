be7b589a312c9cadbac5a1b847910b5df9ad317d
mlyxshi <mlyxdev@gmail.com>
2024-03-24 12:58:32 +0900

    test

--- host/oracle/aarch64/us2.nix
+++ host/oracle/aarch64/us2.nix
@@ -4,7 +4,8 @@ settings = pkgs.writeText "settings.json" ''
   {
     "download-dir": "/var/lib/transmission/files",
     "rpc-whitelist-enabled": false,
-    "rpc-authentication-required": true
+    "rpc-authentication-required": true,
+    "rpc-bind-address" = "192.168.15.1"
   }
 '';
 in
@@ -17,7 +18,7 @@ in
   networking.nftables.enable = lib.mkForce false;
 
 
-vpnnamespaces.wg = {
+  vpnnamespaces.wg = {
     enable = true;
     accessibleFrom = [
       "192.168.0.0/24"
@@ -35,72 +36,54 @@ vpnnamespaces.wg = {
     vpnnamespace = "wg";
   };
 
-  services.transmission = {
-    enable = true;
-    settings = {
-      "rpc-bind-address" = "192.168.15.1"; # Bind RPC/WebUI to bridge address
+  users = {
+    users.transmission = {
+      group = "transmission";
+      isNormalUser = true;
     };
-  }; 
-
-  # users = {
-  #   users.transmission = {
-  #     group = "transmission";
-  #     isNormalUser = true;
-  #   };
-  #   groups.transmission = { };
-  # };
-
-  # vpnnamespaces.wg = {
-  #   enable = true;
-  #   accessibleFrom = [
-  #     "192.168.0.0/24"
-  #   ];
-  #   wireguardConfigFile = "/etc/secret/wg0.conf";
-  #   portMappings = [
-  #     # { from = 22; to = 22; } # tcp is default
-  #     # { from = 22; to = 22; protocol = "tcp"; }
-  #     # { from = 8080; to = 80; protocol = "udp"; }
-  #     # { from = 443; to = 443; protocol = "both"; }
-  #   ];
-  # };
-
-  # # Enable and specify VPN namespace to confine service in.
-
-  # # services.transmission = {
-  # #   enable = true;
-  # #   settings = {
-  # #     "rpc-bind-address" = "192.168.15.1"; # Bind RPC/WebUI to bridge address
-  # #   };
-  # # };
-
-  # systemd.services.transmission-init = {
-  #   unitConfig.ConditionPathExists = "!%S/transmission/settings.json";
-  #   script = ''
-  #     cat ${settings} > settings.json
-  #   '';
-  #   serviceConfig.User = "transmission";
-  #   serviceConfig.Type = "oneshot";
-  #   serviceConfig.StateDirectory = "transmission";
-  #   serviceConfig.WorkingDirectory = "%S/transmission";
-  #   wantedBy = [ "multi-user.target" ];
-  # };
-
-  # systemd.services.transmission = {
-  #   after = [ "transmission-init.service" "network-online.target" ];
-  #   wants = [ "network-online.target" ];
-  #   environment = {
-  #     TRANSMISSION_HOME = "%S/transmission";
-  #     TRANSMISSION_WEB_HOME = "${pkgs.transmission}/public_html";
-  #   };
-  #   serviceConfig.User = "transmission";
-  #   serviceConfig.EnvironmentFile = [ "/etc/secret/transmission" ];
-  #   serviceConfig.ExecStart =
-  #     "${pkgs.transmission}/bin/transmission-daemon --foreground --username $ADMIN --password $PASSWORD";
-  #   serviceConfig.WorkingDirectory = "%S/transmission";
-  #   wantedBy = [ "multi-user.target" ];
-  # };
+    groups.transmission = { };
+  };
 
 
+  systemd.services.transmission-init = {
+    unitConfig.ConditionPathExists = "!%S/transmission/settings.json";
+    script = ''
+      cat ${settings} > settings.json
+    '';
+    serviceConfig.User = "transmission";
+    serviceConfig.Type = "oneshot";
+    serviceConfig.StateDirectory = "transmission";
+    serviceConfig.WorkingDirectory = "%S/transmission";
+    wantedBy = [ "multi-user.target" ];
+  };
 
+  systemd.services.transmission = {
+    after = [ "transmission-init.service" "network-online.target" ];
+    wants = [ "network-online.target" ];
+    environment = {
+      TRANSMISSION_HOME = "%S/transmission";
+      TRANSMISSION_WEB_HOME = "${pkgs.transmission}/public_html";
+    };
+    serviceConfig.User = "transmission";
+    serviceConfig.EnvironmentFile = [ "/etc/secret/transmission" ];
+    serviceConfig.ExecStart =
+      "${pkgs.transmission}/bin/transmission-daemon --foreground --username $ADMIN --password $PASSWORD";
+    serviceConfig.WorkingDirectory = "%S/transmission";
+    wantedBy = [ "multi-user.target" ];
+  };
 
+   services.traefik = {
+    dynamicConfigOptions = {
+      http = {
+        routers.transmission = {
+          rule = "Host(`transmission-vpn.${config.networking.domain}`)";
+          entryPoints = [ "websecure" ];
+          service = "transmission";
+        };
+
+        services.transmission.loadBalancer.servers =
+          [{ url = "http://192.168.1.15:9091"; }];
+      };
+    };
+  };
 }
