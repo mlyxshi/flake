a5f2fa560afe072ceaa8758ae42a9ce30edc5ee7
mlyxshi <mlyxdev@gmail.com>
2024-03-24 22:02:25 +0900

    test

--- host/oracle/x86_64/kr2.nix
+++ host/oracle/x86_64/kr2.nix
@@ -1,3 +1,18 @@
-{ self, pkgs, lib, config, ... }: {
-  imports = [ self.nixosModules.services.hydra.x86_64 ];
+{ self, pkgs, lib, config,vpnconfinement, ... }: {
+  imports = [ 
+    self.nixosModules.services.hydra.x86_64 
+    vpnconfinement.nixosModules.default
+  ];
+
+  networking.firewall.enable = lib.mkForce true;
+  networking.nftables.enable = lib.mkForce false;
+
+
+  vpnnamespaces.wg = {
+    enable = true;
+    accessibleFrom = [ "192.168.0.0/24" ];
+    wireguardConfigFile = "/tmp/wg0.conf";
+  };
+
+
 }
