3afacb96e735418cf72bc5e53c7b670f7fc26898
mlyxshi <mlyxdev@gmail.com>
2024-03-26 14:16:37 +0900

    test

--- host/oracle/aarch64/us1.nix
+++ host/oracle/aarch64/us1.nix
@@ -1,9 +1,9 @@
 { self, config, pkgs, lib, ... }: {
   #imports = [ self.nixosModules.services.hysteria ];
 
-  networking.useNetworkd = lib.mkForce false;
-  networking.useDHCP = lib.mkForce true;
+  # networking.useNetworkd = lib.mkForce false;
+  # networking.useDHCP = lib.mkForce true;
 
-  networking.firewall.enable = lib.mkForce true;
-  networking.nftables.enable = lib.mkForce false;
+  # networking.firewall.enable = lib.mkForce true;
+  # networking.nftables.enable = lib.mkForce false;
 }
