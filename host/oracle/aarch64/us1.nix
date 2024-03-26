f8e9e5e2bba3ed2a837e731e8bcc777960b4b315
mlyxshi <mlyxdev@gmail.com>
2024-03-26 14:26:03 +0900

    test

--- host/oracle/aarch64/us1.nix
+++ host/oracle/aarch64/us1.nix
@@ -1,9 +1,16 @@
 { self, config, pkgs, lib, ... }: {
   #imports = [ self.nixosModules.services.hysteria ];
 
-  # networking.useNetworkd = lib.mkForce false;
-  # networking.useDHCP = lib.mkForce true;
+  # I don't know why oracle ipv6 is not working under systemd-networkd
+  # so use default old way to configure network 
+  networking.useNetworkd = lib.mkForce false;
+  networking.useDHCP = lib.mkForce true;
 
-  # networking.firewall.enable = lib.mkForce true;
-  # networking.nftables.enable = lib.mkForce false;
+  networking.firewall.enable = lib.mkForce true;
+  networking.nftables.enable = lib.mkForce false;
+  networking.firewall.allowedTCPPorts = [ 443 80 ];
+
+  
+
+
 }
