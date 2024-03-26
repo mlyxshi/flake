d9e61e2594421326eedc58f69d0b424df34a3883
mlyxshi <mlyxdev@gmail.com>
2024-03-26 12:08:52 +0900

    test

--- host/oracle/aarch64/au.nix
+++ host/oracle/aarch64/au.nix
@@ -1,4 +1,8 @@
 { self, config, pkgs, lib, ... }: {
   imports = [ self.nixosModules.services.prometheus ];
 
+
+  networking.useNetworkd = lib.mkForce false;
+  networking.useDHCP = lib.mkForce true;
+
 }
