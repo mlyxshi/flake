e4cbc3846d4e1f57a43e15e5402b3b667841924b
mlyxshi <mlyxdev@gmail.com>
2024-03-26 16:56:31 +0900

    test

--- host/oracle/aarch64/us1.nix
+++ host/oracle/aarch64/us1.nix
@@ -23,6 +23,52 @@ in {
   networking.nftables.enable = lib.mkForce false;
   networking.firewall.allowedTCPPorts = [ 443 80 ];
 
+  systemd.services.vpn = {
+    after = [ "network-online.target" ];
+    before = [ "transmission.service" ];
+    wants = [ "network-online.target" ];
+    environment.WG_CONFIG_FILE = "/tmp/wg1.conf";
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+      ExecStart = pkgs.writeShellScript "transmission.sh" ''
+        export PATH=$PATH:${pkgs.wireguard-tools}/bin:${pkgs.iproute2}/bin
+
+        # Create a new network namespace
+        ip netns add vpn
+      
+        # Create wireguard interface in host network namespace(get default route)
+        # then move it to vpn network namespace
+        ip link add wg1 type wireguard
+        ip link set wg1 netns vpn
+
+        # Add address to wireguard interface
+        ADDRESS_LINE=$(grep "Address" $WG_CONFIG_FILE)
+        ADDRESS=$(echo $ADDRESS_LINE | cut -d '=' -f 2| tr -d ' ')
+        IFS=',' read -ra PARTS <<< "$ADDRESS"
+        for PART in "''${PARTS[@]}"; do
+          ip -n vpn a add $PART dev wg1
+          ip -n vpn a add $PART dev wg1
+        done
+
+        # Add wireguard configuration
+        ip netns exec vpn wg setconf wg1 <(wg-quick strip $WG_CONFIG_FILE)
+
+        # Up the interface
+        ip -n vpn link set lo up
+        ip -n vpn link set wg1 up
+
+        # Add default route
+        ip -n vpn route add default dev wg1
+        ip -n vpn -6 route add default dev wg1
+      '';
+
+      ExecStopPost = "${pkgs.iproute2}/bin/ip netns del vpn"
+    };
+
+    wantedBy = [ "multi-user.target" ];
+  };
+
   users = {
     users.transmission = {
       group = "transmission";
@@ -60,7 +106,7 @@ in {
     serviceConfig.ExecStart =
       "${pkgs.transmission}/bin/transmission-daemon --foreground --username $ADMIN --password $PASSWORD";
     serviceConfig.WorkingDirectory = "%S/transmission";
-    serviceConfig.NetworkNamespacePath ="/run/netns/vpn";
+    serviceConfig.NetworkNamespacePath = "/run/netns/vpn";
     wantedBy = [ "multi-user.target" ];
   };
 
