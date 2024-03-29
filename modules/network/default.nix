67d8b6978f6c704ec82dde1e2ff4daf66207d848
mlyxshi <mlyxdev@gmail.com>
2024-03-29 16:43:18 +0900

    systemd-netword

--- modules/network/default.nix
+++ modules/network/default.nix
@@ -4,6 +4,7 @@
   networking.dhcpcd.enable = false;
 
   systemd.network.enable = true;
+  systemd.services.systemd-networkd.environment.SYSTEMD_LOG_LEVEL = "debug";
   systemd.network.wait-online.anyInterface = true;
   systemd.network.networks.ethernet-default-dhcp = {
     matchConfig = { Name = [ "en*" "eth*" ]; };
