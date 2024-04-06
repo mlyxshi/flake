13b76fa6f30bfd1c35be067a53cbeed45629da5e
mlyxshi <mlyxdev@gmail.com>
2024-04-06 12:15:03 +0900

    flake update

--- host/oracle/aarch64/jp2.nix
+++ host/oracle/aarch64/jp2.nix
@@ -1,6 +1,6 @@
 { self, pkgs, lib, config, ... }: {
   imports = [
     self.nixosModules.services.hysteria
-    self.nixosModules.containers.netboot-tftp
+    # self.nixosModules.containers.netboot-tftp
   ];
 }
