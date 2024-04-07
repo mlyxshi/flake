77e7abbdf4d58603d8c017c2ff311cfcf04704b0
mlyxshi <mlyxdev@gmail.com>
2024-04-07 21:24:16 +0900

    test

--- home/desktop.nix
+++ home/desktop.nix
@@ -1,6 +1,12 @@
 { pkgs, lib, config, self, ... }: {
 
-  imports = [ ./. ./firefox ./mpv.nix ./xremap.nix ./kde.nix ];
+  imports = [ 
+    ./. 
+    ./firefox 
+    ./mpv.nix 
+    # ./xremap.nix 
+    ./kde.nix 
+  ];
 
   nixpkgs.overlays = [ self.overlays.default ];
 
--- modules/os/nixos/desktop.nix
+++ modules/os/nixos/desktop.nix
@@ -40,10 +40,8 @@
 
   services.xserver.enable = true;
   services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
+  services.desktopManager.plasma6.enable = true;
 
-  #services.displayManager.sddm.enable = true;
-  #services.desktopManager.plasma6.enable = true;
 
   services.flatpak.enable = true;
 
