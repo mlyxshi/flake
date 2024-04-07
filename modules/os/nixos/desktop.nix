30a1ccf35df69a0e0359665cd272db522483a60c
mlyxshi <mlyxdev@gmail.com>
2024-04-07 20:01:38 +0900

    test

--- modules/os/nixos/desktop.nix
+++ modules/os/nixos/desktop.nix
@@ -37,11 +37,13 @@
     };
   };
 
-  #services.displayManager.sddm.enable = true;
+
   services.xserver.enable = true;
   services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
 
-  services.desktopManager.plasma6.enable = true;
+  #services.displayManager.sddm.enable = true;
+  #services.desktopManager.plasma6.enable = true;
 
   services.flatpak.enable = true;
 
