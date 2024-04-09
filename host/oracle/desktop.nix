3a8acc819bbc44184620a7c574de1d1d7121040e
mlyxshi <mlyxdev@gmail.com>
2024-04-09 21:47:30 +0900

    test

--- host/oracle/desktop.nix
+++ host/oracle/desktop.nix
@@ -1,3 +1,8 @@
+# use oracle vnc console to bootstrap 
+# add autostart app krfb
+# enegry saving: turn off screen OFF
+# Wait Fix: https://discuss.kde.org/t/krfb-on-wayland-have-to-confirm-remote-control-requested/2650
+# Temp Fix: just do not reboot after initial setup
 { self, nixpkgs, home-manager, secret, plasma-manager }:
 nixpkgs.lib.nixosSystem {
   modules = [
