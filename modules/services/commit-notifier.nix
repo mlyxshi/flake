e19648b127eda25f279d8a6d4ec2e0442e6bca35
mlyxshi <mlyxdev@gmail.com>
2024-04-10 19:47:47 +0900

    test

---
+++ modules/services/commit-notifier.nix
@@ -0,0 +1,25 @@
+{ pkgs, lib, config, ... }: {
+  systemd.services.commit-notifier = {
+    after = [ "commit-notifier-install.service" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig.EnvironmentFile = [ "/secret/commit-notifier" ];
+    serviceConfig.ExecStart = ''
+      /root/.nix-profile/bin/commit-notifier --working-dir /var/lib/commit-notifier/  --cron "0 */5 * * * *"'';
+  };
+
+  # Too many flake input will add to my config if add linyinfeng/commit-notifier to my config
+  # Just install it without effecting my config (bad way)
+  systemd.services.commit-notifier-install = {
+    after = [ "network-online.target" ];
+    wantedBy = [ "multi-user.target" ];
+    unitConfig.ConditionPathExists = "!/root/.nix-profile/bin/commit-notifier";
+    serviceConfig.Type = "oneshot";
+    serviceConfig.ExecStart =
+      "/run/current-system/sw/bin/nix profile install github:linyinfeng/commit-notifier#commit-notifier";
+  };
+
+  systemd.tmpfiles.settings."10-commit-notifier" = {
+    "/var/lib/commit-notifier/337000294".d = { };
+  };
+
+}
