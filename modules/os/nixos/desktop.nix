12a8f415a4b747597abcd6b57d7b32e77b098af4
mlyxshi <mlyxdev@gmail.com>
2024-04-12 11:50:57 +0900

    remove x11

--- modules/os/nixos/desktop.nix
+++ modules/os/nixos/desktop.nix
@@ -42,8 +42,7 @@
   };
 
 
-  services.xserver.enable = true;
-  services.xserver.displayManager.sddm.enable = true;
+  services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
 
 
