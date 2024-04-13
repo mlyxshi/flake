d1a9b93623b5a7d2da2fa54b8ae29a5de258954e
mlyxshi <mlyxdev@gmail.com>
2024-04-13 15:56:53 +0900

    clean

--- pkgs/commit-notifier/default.nix
+++ pkgs/commit-notifier/default.nix
@@ -1,14 +1,13 @@
 { rustPlatform, fetchFromGitHub, pkg-config, openssl, sqlite }:
-
 rustPlatform.buildRustPackage {
   pname = "commit-notifier";
   version = "unstable-2024-04-13";
-  src = fetchFromGitHub ({
+  src = fetchFromGitHub {
     owner = "linyinfeng";
     repo = "commit-notifier";
     rev = "fb2871be21823b583ecafceb141eef73f1cf50fb";
     sha256 = "sha256-gnYsTbppEiB9Tk9eUdmUWiwxtR6pwJwaLic768ayNx4=";
-  });
+  };
 
   cargoSha256 = "sha256-rGabRsQMC9TwiPbxOXDigMnGWy4SN1z1Vkcv1vPPLmU=";
 
