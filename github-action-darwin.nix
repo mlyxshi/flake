03c504325fa0e3f815e0e1ab6cc515f816f43a7b
mlyxshi <mlyxdev@gmail.com>
2024-04-14 15:33:14 +0900

    clean

--- github-action-darwin.nix
+++ github-action-darwin.nix
@@ -5,7 +5,9 @@
   users.users.runner.home = "/Users/runner";
   users.users.runner.openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMpaY3LyCW4HHqbp4SA4tnA+1Bkgwrtro2s/DEsBcPDe" ];
 
-  environment.systemPackages = [
+  environment.systemPackages = with pkgs;[
+    helix
+    joshuto
   ];
 
   nix = {
