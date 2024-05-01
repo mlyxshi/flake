03809a0f911eae29333a83e79593460d31e82660
mlyxshi <mlyxdev@gmail.com>
2024-05-01 12:16:40 +0900

    clean

--- home/kde.nix
+++ home/kde.nix
@@ -2,17 +2,17 @@
 
   imports = [ plasma-manager.homeManagerModules.plasma-manager ];
 
-  home.file.".config/kate/lspclient/settings.json".text = ''
-    {
-      "servers": {
-        "nix": {
-          "command": ["nil"],
-          "url": "https://github.com/oxalica/nil",
-          "highlightingModeRegex": "^Nix$"
-        }
-      }
-    }
-  '';
+  # home.file.".config/kate/lspclient/settings.json".text = ''
+  #   {
+  #     "servers": {
+  #       "nix": {
+  #         "command": ["nil"],
+  #         "url": "https://github.com/oxalica/nil",
+  #         "highlightingModeRegex": "^Nix$"
+  #       }
+  #     }
+  #   }
+  # '';
 
   programs.plasma = {
     enable = true;
