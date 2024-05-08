c4855f6b3f5add55b3dcdcf208162f3d7efde358
mlyxshi <mlyxdev@gmail.com>
2024-05-08 20:08:20 +0900

    clean

--- host/oracle/jp2.nix
+++ host/oracle/jp2.nix
@@ -16,26 +16,4 @@
   system.switch.enable = false;
   system.etc.overlay.enable = true;
   systemd.sysusers.enable = true;
-
-  systemd.tmpfiles.settings."10-boot" =
-    let
-      efiArch = pkgs.stdenv.hostPlatform.efiArch;
-    in
-    { 
-      "/boot/EFI".d = { };
-      "/boot/EFI/BOOT".d = { };
-      "/boot/EFI/BOOT/BOOT${lib.toUpper efiArch}.EFI".C.argument = "${pkgs.systemd}/lib/systemd/boot/efi/systemd-boot${efiArch}.efi";
-
-      "/boot/EFI/nixos/".d = { };
-      "/boot/EFI/nixos/kernel.efi".C.argument = "${config.boot.kernelPackages.kernel}/${config.system.boot.loader.kernelFile}";
-      "/boot/EFI/nixos/initrd.efi".C.argument = "${config.system.build.initialRamdisk}/${config.system.boot.loader.initrdFile}";
-
-      "/boot/loader/entries".d = { };
-      "/boot/loader/entries/nixos.conf".C.argument = pkgs.writeText "nixos.conf" ''
-        title NixOS
-        linux /EFI/nixos/kernel.efi
-        initrd /EFI/nixos/initrd.efi
-        options init=${config.system.build.toplevel}/init ${toString config.boot.kernelParams}
-      '';
-    };
 }
