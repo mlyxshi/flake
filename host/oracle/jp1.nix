e1e972c16e2889d54127792569e3dde0e00d6b89
mlyxshi <mlyxdev@gmail.com>
2024-05-08 21:48:32 +0900

    clean

--- host/oracle/jp1.nix
+++ host/oracle/jp1.nix
@@ -4,7 +4,16 @@
     self.nixosModules.containers.auto-bangumi
   ];
 
-  services.atftpd.enable = true;
-  services.atftpd.root = "/var/lib/tftp";
+  virtualisation.oci-containers.containers.netboot-tftp = {
+    image = "docker.io/langren1353/netboot-shell-tftp";
+    ports = [ "69:69/udp" ];
+    environment = {
+      "PUID" = "1111";
+      "PGID" = "1112";
+    };
+  };
+
+  # services.atftpd.enable = true;
+  # services.atftpd.root = "/var/lib/tftp";
   # Since atftpd run as the nobody user, the permission of the directory must be set properly to allow file reading and writing.
 }
