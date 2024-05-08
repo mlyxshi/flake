ada29b5cdc4b7e8be2c83fa0e16dc3494b86e6a6
mlyxshi <mlyxdev@gmail.com>
2024-05-08 19:47:28 +0900

    clean

--- host/oracle/jp2.nix
+++ host/oracle/jp2.nix
@@ -30,15 +30,15 @@
       "/boot/EFI/BOOT/BOOT${lib.toUpper efiArch}.EFI".C.argument = "${pkgs.systemd}/lib/systemd/boot/efi/systemd-boot${efiArch}.efi";
 
       "/boot/EFI/nixos/".d = { };
-      "/boot/EFI/nixos/kernel.efi".C.argument = "${config.boot.kernelPackages.kernel}/${config.system.boot.loader.kernelFile}";
-      "/boot/EFI/nixos/initrd.efi".C.argument = "${config.system.build.initialRamdisk}/${config.system.boot.loader.initrdFile}";
+      # "/boot/EFI/nixos/kernel.efi".C.argument = "${config.boot.kernelPackages.kernel}/${config.system.boot.loader.kernelFile}";
+      # "/boot/EFI/nixos/initrd.efi".C.argument = "${config.system.build.initialRamdisk}/${config.system.boot.loader.initrdFile}";
 
       "/boot/loader/entries".d = { };
-      "/boot/loader/entries/nixos.conf".C.argument = pkgs.writeText "nixos.conf" ''
-        title NixOS
-        linux /EFI/nixos/kernel.efi
-        initrd /EFI/nixos/initrd.efi
-        options init=${config.system.build.toplevel}/init ${toString config.boot.kernelParams}
-      '';
+      # "/boot/loader/entries/nixos.conf".C.argument = pkgs.writeText "nixos.conf" ''
+      #   title NixOS
+      #   linux /EFI/nixos/kernel.efi
+      #   initrd /EFI/nixos/initrd.efi
+      #   options init=${config.system.build.toplevel}/init ${toString config.boot.kernelParams}
+      # '';
     };
 }
