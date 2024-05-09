96ddb19497dade37fec2d8e2674989b642c8c8b1
mlyxshi <mlyxdev@gmail.com>
2024-05-09 17:24:46 +0900

    clean

--- modules/strip/default.nix
+++ modules/strip/default.nix
@@ -2,13 +2,13 @@
 { pkgs, lib, config, ... }: {
   # https://nixos.org/manual/nixos/unstable/#sec-replace-modules
   disabledModules = [
-
+    "services/network-filesystems/samba.nix"
   ];
 
   imports = [ ];
 
   programs.nano.enable = false;
-  programs.less.enable = lib.mkForce false; 
+  programs.less.enable = lib.mkForce false;
   programs.command-not-found.enable = false;
   boot.bcache.enable = false;
   services.lvm.enable = false;
