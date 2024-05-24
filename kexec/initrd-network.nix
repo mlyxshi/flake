b7b0c3f33463cb0998cd096e5e1e19306dc17e2d
mlyxshi <mlyxdev@gmail.com>
2024-05-24 09:46:28 +0900

    systemd-resolved in kexec mergerd

--- kexec/initrd-network.nix
+++ kexec/initrd-network.nix
@@ -7,16 +7,6 @@
     networkConfig = { DHCP = "yes"; };
   };
 
-  # systemd-resolved
-  boot.initrd.systemd.users.systemd-resolve = { };
-  boot.initrd.systemd.groups.systemd-resolve = { };
-  boot.initrd.systemd.additionalUpstreamUnits = [ "systemd-resolved.service" ];
-  boot.initrd.systemd.storePaths = [ "${config.boot.initrd.systemd.package}/lib/systemd/systemd-resolved" ];
-  boot.initrd.systemd.services.systemd-resolved = {
-    wantedBy = [ "initrd.target" ];
-    serviceConfig.ExecStartPre = "-+/bin/ln -s /run/systemd/resolve/resolv.conf /etc/resolv.conf";
-  };
-
   # sshd
   boot.initrd.network.ssh.enable = true;
   boot.initrd.network.ssh.authorizedKeys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMpaY3LyCW4HHqbp4SA4tnA+1Bkgwrtro2s/DEsBcPDe" ];
