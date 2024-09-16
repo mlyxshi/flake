713540ae1e205e30da65bf589975c668fef58a28
mlyxshi <mlyxdev@gmail.com>
2024-09-16 18:25:02 +0900

    update

--- host/oracle/jp2.nix
+++ host/oracle/jp2.nix
@@ -3,14 +3,4 @@
     self.nixosModules.services.transmission
   ];
 
-  systemd.services.applestore = {
-    after = [ "network.target" ];
-    wantedBy = [ "multi-user.target" ];
-
-    serviceConfig = {
-      Restart = "always";
-      ExecStart = "/root/.nix-profile/bin/java -jar /apple-monitor-v0.1.1.jar";
-    };
-  };
-
 }
