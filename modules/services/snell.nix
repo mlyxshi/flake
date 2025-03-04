701bb94d16c48f47d077e6698ed6d58c3992f082
mlyxshi <mlyxdev@gmail.com>
2025-03-05 01:31:24 +0800

    update

--- modules/services/snell.nix
+++ modules/services/snell.nix
@@ -1,14 +1,19 @@
 { config, pkgs, lib, ... }: {
   programs.nix-ld.enable = true;
+  
   systemd.services.snell = {
     after = [ "network.target" ];
     wantedBy = [ "multi-user.target" ];
     preStart = ''
-      ${pkgs.wget}/bin/wget https://dl.nssurge.com/snell/snell-server-v4.1.1-linux-amd64.zip
-      ${pkgs.unzip}/bin/unzip snell-server-v4.1.1-linux-amd64.zip
+      if [ ! -f /var/lib/snell/snell-server ]; then
+        ${pkgs.wget}/bin/wget https://dl.nssurge.com/snell/snell-server-v4.1.1-linux-amd64.zip
+        ${pkgs.unzip}/bin/unzip snell-server-v4.1.1-linux-amd64.zip
+      fi
     '';
-    serviceConfig.ExecStart = "/var/lib/snell-server -c /secret/snell";
+    serviceConfig.ExecStart = "/var/lib/snell/snell-server -c /secret/snell";
     serviceConfig.StateDirectory = "snell";
     serviceConfig.WorkingDirectory = "%S/snell";
   };
+
+
 }
