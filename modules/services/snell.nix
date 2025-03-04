3859c3845fd702d5e54b54e93bfcde871a55c0ae
mlyxshi <mlyxdev@gmail.com>
2025-03-05 01:27:29 +0800

    update

--- modules/services/snell.nix
+++ modules/services/snell.nix
@@ -7,7 +7,7 @@
       ${pkgs.wget}/bin/wget https://dl.nssurge.com/snell/snell-server-v4.1.1-linux-amd64.zip
       ${pkgs.unzip}/bin/unzip snell-server-v4.1.1-linux-amd64.zip
     '';
-    serviceConfig.ExecStart = "./snell-server -c /secret/snell";
+    serviceConfig.ExecStart = "/var/lib/snell-server -c /secret/snell";
     serviceConfig.StateDirectory = "snell";
     serviceConfig.WorkingDirectory = "%S/snell";
   };
