c8f1c91cc4f05ef39e4df2ae93c1a124dbbfe8ba
mlyxshi <mlyxdev@gmail.com>
2025-03-08 17:49:48 +0800

    update

--- modules/services/nezha.nix
+++ modules/services/nezha.nix
@@ -2,5 +2,6 @@
   services.nezha-agent.enable = true;
   services.nezha-agent.settings.server = "top.mlyxshi.com:8008";
   services.nezha-agent.genUuid = true;
+  services.nezha-agent.settings.temperature = false;
   services.nezha-agent.clientSecretFile = "/secret/nezha";
 }
