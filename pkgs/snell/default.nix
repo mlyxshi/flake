3694467374d43f2e1c8422410cd1dd573f9ddbd8
mlyxshi <mlyxdev@gmail.com>
2025-03-10 15:28:38 +0800

    update

--- pkgs/snell/default.nix
+++ pkgs/snell/default.nix
@@ -1,22 +1,22 @@
-{ stdenvNoCC, fetchzip }:
-stdenvNoCC.mkDerivation rec {
+{ stdenv, fetchzip }:
+stdenv.mkDerivation rec {
   pname = "snell";
   version = "4.1.1";
 
   src = fetchzip {
     url =
-      if stdenvNoCC.hostPlatform.isx86_64
+      if stdenv.hostPlatform.isx86_64
       then "https://dl.nssurge.com/snell/snell-server-v${version}-linux-amd64.zip"
       else "https://dl.nssurge.com/snell/snell-server-v${version}-linux-aarch64.zip";
     hash =
-      if stdenvNoCC.hostPlatform.isx86_64
+      if stdenv.hostPlatform.isx86_64
       then "sha256-IcW13oq2SC+XeCwUVU2ZVkjYe0V29gczYFz+YXhZgWU="
       else "sha256-ogZBC/Bjo7sdZlKQz+5T/JCPAUS4Ce4n99G3oMdbUe4=";
   };
 
   nativeBuildInputs = [ autoPatchelfHook ];
   buildInputs = [ stdenv.cc.cc ];
-  
+
   installPhase = ''
     mkdir -p $out/bin
     cp snell-server $out/bin/snell-server
