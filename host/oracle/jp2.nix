f7333c4ba975f52d3d551d4aee76e7920cec8abc
mlyxshi <mlyxdev@gmail.com>
2025-03-12 17:16:37 +0800

    update

--- host/oracle/jp2.nix
+++ host/oracle/jp2.nix
@@ -1,6 +1,21 @@
-{ self, pkgs, lib, config, modulesPath, ... }: {
+{ self, pkgs, lib, config, ... }: {
   imports = [
+    self.nixosModules.services.prometheus
     self.nixosModules.services.transmission
+    
+    self.nixosModules.containers.podman
+    self.nixosModules.containers.miniflux
   ];
 
+  environment.systemPackages = with pkgs; [
+    nix-index
+
+    (pkgs.writeShellScriptBin "update-index" ''
+      filename="index-$(uname -m | sed 's/^arm64$/aarch64/')-$(uname | tr A-Z a-z)"
+      mkdir -p ~/.cache/nix-index && cd ~/.cache/nix-index
+      # -N will only download a new version if there is an update.
+      wget -q -N https://github.com/Mic92/nix-index-database/releases/latest/download/$filename
+      ln -f $filename files
+    '')
+  ];
 }
