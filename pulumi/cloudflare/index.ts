9407817ba632d6bce40bc70703397af72107e701
mlyxshi <mlyxdev@gmail.com>
2025-03-18 00:14:37 +0800

    update

--- pulumi/cloudflare/index.ts
+++ pulumi/cloudflare/index.ts
@@ -5,6 +5,7 @@ const dns: Record<string, string[]> = {
     "138.2.16.45": ["jp2", "transmission-jp2", "transmission-jp2-index", "changeio", "miniflux", "rsshub", "alert", "metric"],
     "155.248.196.71": ["us", "transmission-us", "transmission-us-index"],
     "144.34.239.82": ["lax"],
+    "45.89.235.150": ["hnd"],
 }
 
 Object.keys(dns).forEach(ip => {
