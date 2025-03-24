03a35bb52297b50b88485cd51f6a46ebfc7845fa
mlyxshi <mlyxdev@gmail.com>
2025-03-24 22:29:02 +0800

    update

--- host/bios/pvg/forward.nix
+++ host/bios/pvg/forward.nix
@@ -10,17 +10,15 @@
       chain PREROUTING {
         type nat hook prerouting priority -100; policy accept;
         tcp dport 5555 dnat to 47.242.243.176:8888 
-        tcp dport 9999 dnat to 47.242.243.176:5201
 
-        tcp dport 6666 dnat to 47.245.33.67:8888
+        tcp dport 1111 dnat to 47.242.243.176:1112
       }
 
       chain POSTROUTING {
         type nat hook postrouting priority 100; policy accept;
         ip daddr 47.242.243.176 tcp dport 8888 masquerade
-        ip daddr 47.242.243.176 tcp dport 5201 masquerade
 
-        ip daddr 47.245.33.67 tcp dport 8888 masquerade
+        ip daddr 47.242.243.176 tcp dport 1112 masquerade
       }
     }
   '';
