cb6ffe983273f161c04cc25070b0f228570fe76d
mlyxshi <mlyxdev@gmail.com>
2025-03-30 23:37:33 +0800

    update

--- host/oracle/jp1.nix
+++ host/oracle/jp1.nix
@@ -3,7 +3,7 @@
     self.nixosModules.services.tftp
     self.nixosModules.services.hysteria
     self.nixosModules.services.beszel-hub
-
+    self.nixosModules.services.snell  
     self.nixosModules.containers.podman
   ];
 }
