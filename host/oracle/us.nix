c6d1048ddf040b98454f31165c7f88bd1a1a3b01
mlyxshi <mlyxdev@gmail.com>
2025-03-31 00:30:08 +0800

    update

--- host/oracle/us.nix
+++ host/oracle/us.nix
@@ -1,8 +1,7 @@
 { self, config, pkgs, lib, ... }: {
   imports = [
-    self.nixosModules.services.hysteria
+    # self.nixosModules.services.hysteria
     self.nixosModules.services.transmission
     self.nixosModules.services.snell
-    self.nixosModules.containers.podman
   ];
 }
