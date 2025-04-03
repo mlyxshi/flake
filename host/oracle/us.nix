2ea4eb8f695eb4258d0f7d495494c628beaecac5
mlyxshi <mlyxdev@gmail.com>
2025-04-03 12:20:49 +0800

    update

--- host/oracle/us.nix
+++ host/oracle/us.nix
@@ -3,4 +3,24 @@
     self.nixosModules.services.transmission
     self.nixosModules.services.snell
   ];
+
+  boot.kernel.sysctl = {
+    "net.ipv4.ip_forward" = 1;
+  };
+
+  # Oracle US to JP(China Telecom to Oracle SJC via AS4134)
+  networking.nftables.enable = true;
+  networking.nftables.ruleset = ''
+    table ip REDIRECT {
+      chain PREROUTING {
+        type nat hook prerouting priority -100; policy accept;
+        tcp dport 5555 dnat to 138.3.223.82:8888
+      }
+
+      chain POSTROUTING {
+        type nat hook postrouting priority 100; policy accept;
+        ip daddr 138.3.223.82 tcp dport 8888 masquerade
+      }
+    }
+  '';
 }
