e7b942303064310aa68fc3031d946ad260927178
mlyxshi <mlyxdev@gmail.com>
2025-04-05 18:51:55 +0800

    update

--- host/oracle/jp1.nix
+++ host/oracle/jp1.nix
@@ -10,18 +10,18 @@
     "net.ipv4.ip_forward" = 1;
   };
 
-  # Oracle JP to Webarena(IP Unlock)
+  # Oracle JP to US
   networking.nftables.enable = true;
   networking.nftables.ruleset = ''
     table ip REDIRECT {
       chain PREROUTING {
         type nat hook prerouting priority -100; policy accept;
-        tcp dport 5555 dnat to 140.227.176.82:8888 
+        tcp dport 5555 dnat to 155.248.196.71:8888 
       }
 
       chain POSTROUTING {
         type nat hook postrouting priority 100; policy accept;
-        ip daddr 140.227.176.82 tcp dport 8888 masquerade
+        ip daddr 155.248.196.71 tcp dport 8888 masquerade
       }
     }
   '';
