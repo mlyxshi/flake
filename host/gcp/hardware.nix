79a91f0d4560032fdc2d1e527a495e3e8d26b75d
mlyxshi <mlyxdev@gmail.com>
2025-04-21 16:27:47 +0800

    update

--- host/gcp/hardware.nix
+++ host/gcp/hardware.nix
@@ -7,7 +7,7 @@
   boot.initrd.supportedFilesystems = [ "ext4" ];
 
   boot.loader.systemd-boot.enable = true;
-  boot.loader.systemd-boot.configurationLimit = 3;
-  boot.loader.timeout = 3;
+  boot.loader.systemd-boot.configurationLimit = 2;
+  boot.loader.timeout = 1;
   boot.loader.efi.canTouchEfiVariables = true;
 }
---
+++ host/gcp/hk/default.nix
@@ -0,0 +1,20 @@
+{ self, nixpkgs, secret }:
+nixpkgs.lib.nixosSystem {
+  modules = [
+    secret.nixosModules.default
+    self.nixosModules.nixos.server
+    self.nixosModules.network
+    ./hardware.nix
+    
+    self.nixosModules.services.snell  
+    self.nixosModules.services.beszel-agent
+    {
+      nixpkgs.overlays = [ self.overlays.default ];
+      nixpkgs.hostPlatform = "x86_64-linux";
+      networking.hostName = "gcp-hk";
+      networking.domain = "mlyxshi.com";
+      services.getty.autologinUser = "root";
+    }
+  ];
+  specialArgs = { inherit self; };
+}
