ecd86d5b6b193e0c30c456dc9278984c03d062c5
mlyxshi <mlyxdev@gmail.com>
2025-05-09 13:00:36 +0800

    update

--- host/gcp/gcp-hk.nix
+++ host/gcp/gcp-hk.nix
@@ -2,21 +2,26 @@
   imports = [
   ];
 
-  boot.kernel.sysctl = {
-    "net.ipv6.conf.all.forwarding" = 1;
-  };
+  # Alice DNS UnlocK Service
+  networking.nameservers = [
+    "181.215.6.75"
+  ]
 
-  networking.nftables.enable = true;
-  networking.nftables.ruleset = ''
-    table ip6 REDIRECT {
-      chain PREROUTING {
-        type nat hook prerouting priority -100; policy accept;
-        tcp dport 8080 dnat to [2a14:67c0:306:7d::a]:8888
-      }
-      chain POSTROUTING {
-        type nat hook postrouting priority 100; policy accept;
-        tcp dport 8888 masquerade
-      }
-    }
-  '';
+  # boot.kernel.sysctl = {
+  #   "net.ipv6.conf.all.forwarding" = 1;
+  # };
+
+  # networking.nftables.enable = true;
+  # networking.nftables.ruleset = ''
+  #   table ip6 REDIRECT {
+  #     chain PREROUTING {
+  #       type nat hook prerouting priority -100; policy accept;
+  #       tcp dport 8080 dnat to [2a14:67c0:306:7d::a]:8888
+  #     }
+  #     chain POSTROUTING {
+  #       type nat hook postrouting priority 100; policy accept;
+  #       tcp dport 8888 masquerade
+  #     }
+  #   }
+  # '';
 }
