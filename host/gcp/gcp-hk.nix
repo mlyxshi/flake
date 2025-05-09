22a4d6187328f045f7fb348d6e718ff59a22b8d8
mlyxshi <mlyxdev@gmail.com>
2025-05-09 12:38:26 +0800

    update

--- host/gcp/gcp-hk.nix
+++ host/gcp/gcp-hk.nix
@@ -2,7 +2,6 @@
   imports = [
   ];
 
-
   boot.kernel.sysctl = {
     "net.ipv6.conf.all.forwarding" = 1;
   };
@@ -10,11 +9,14 @@
   networking.nftables.enable = true;
   networking.nftables.ruleset = ''
     table ip6 REDIRECT {
-      chain prerouting {
+      chain PREROUTING {
         type nat hook prerouting priority -100; policy accept;
-        tcp dport 5555 dnat to [2a14:67c0:306:7d::a]:8888
+        tcp dport 8080 dnat to [2a14:67c0:306:7d::a]:8888
+      }
+      chain POSTROUTING {
+        type nat hook postrouting priority 100; policy accept;
+        tcp dport 8888 masquerade
       }
     }
   '';
-
 }
