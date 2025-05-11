cfe3f5fb2fbfdda2cd10ac3e180df217763801b9
mlyxshi <mlyxdev@gmail.com>
2025-05-11 16:53:00 +0800

    update

--- host/bios/dmit/default.nix
+++ host/bios/dmit/default.nix
@@ -14,7 +14,6 @@ nixpkgs.lib.nixosSystem {
       nixpkgs.hostPlatform = "x86_64-linux";
       networking.hostName = "nrt";
       networking.domain = "mlyxshi.com";
-      services.getty.autologinUser = "root";
     }
   ];
   specialArgs = { inherit self; };
