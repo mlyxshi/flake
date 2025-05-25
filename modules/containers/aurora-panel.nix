e92e16b42fec604050aff3d3b1bdd1a260ffb6b9
mlyxshi <mlyxdev@gmail.com>
2025-05-25 15:10:51 +0800

    update

--- modules/containers/aurora-panel.nix
+++ modules/containers/aurora-panel.nix
@@ -21,7 +21,7 @@
     };
     volumes = [ 
       "/var/lib/aurora/app:/app/ansible/priv_data_dirs" 
-      "/var/lib/aurora/ssh:/app/ansible/env"
+      "/var/lib/aurora/ssh_key:/app/ansible/env/ssh_key"
     ];
     dependsOn = [
       "postgres"
