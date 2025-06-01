42314dea845c065dd12040acc1d4b8a582aae0b1
mlyxshi <mlyxdev@gmail.com>
2025-06-01 15:49:18 +0800

    update

--- host/dmit/shinagawa.nix
+++ host/dmit/shinagawa.nix
@@ -9,6 +9,16 @@
     };
   };
 
+  systemd.network.networks.ethernet-static-v6 = {
+    matchConfig = {
+      Name = "eth0";
+    };
+    networkConfig = {
+      Address = "2403:18c0:1001:179:787a:20ff:fe99:43a5/64";
+      Gateway = "2403:18c0:1001:179::1";
+    };
+  };
+
   services.vnstat.enable = true;
   # Traffic Reset Date
   environment.etc."vnstat.conf".text = ''
--- host/rfc/hk.nix
+++ host/rfc/hk.nix
@@ -10,16 +10,6 @@
     };
   };
 
-  systemd.network.networks.ethernet-static-v6 = {
-    matchConfig = {
-      Name = "eth0";
-    };
-    networkConfig = {
-      Address = "2403:18c0:1001:179:787a:20ff:fe99:43a5/64";
-      Gateway = "2403:18c0:1001:179::1";
-    };
-  };
-
   # Port 22 for FCC
   services.openssh.ports = [ 2222 ];
 }
