7bf0db7cdac9818659d35094b4aeeff95d948344
mlyxshi <mlyxdev@gmail.com>
2025-06-01 15:01:58 +0800

    update

--- host/dmit/shinagawa.nix
+++ host/dmit/shinagawa.nix
@@ -9,8 +9,6 @@
     };
   };
 
-  networking.extraHosts = "2606:4700:4700::1001 api.twitter.com";
-
   services.vnstat.enable = true;
   # Traffic Reset Date
   environment.etc."vnstat.conf".text = ''
