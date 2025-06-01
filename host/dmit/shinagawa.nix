680b3c3c6c16c72225049925d93a6a7d5b8c9182
mlyxshi <mlyxdev@gmail.com>
2025-06-01 15:58:09 +0800

    update

--- host/dmit/shinagawa.nix
+++ host/dmit/shinagawa.nix
@@ -4,20 +4,26 @@
       Name = "eth0";
     };
     networkConfig = {
-      Address = "103.117.103.126/24";
-      Gateway = "103.117.103.1";
+      Address = [
+        "103.117.103.126/24"
+        "2403:18c0:1001:179:787a:20ff:fe99:43a5/64"
+      ];
+      Gateway = [
+        "103.117.103.1"
+        "2403:18c0:1001:179::1"
+      ];
     };
   };
 
-  systemd.network.networks.ethernet-static-v6 = {
-    matchConfig = {
-      Name = "eth0";
-    };
-    networkConfig = {
-      Address = "2403:18c0:1001:179:787a:20ff:fe99:43a5/64";
-      Gateway = "2403:18c0:1001:179::1";
-    };
-  };
+  # systemd.network.networks.ethernet-static-v6 = {
+  #   matchConfig = {
+  #     Name = "eth0";
+  #   };
+  #   networkConfig = {
+  #     Address = "2403:18c0:1001:179:787a:20ff:fe99:43a5/64";
+  #     Gateway = "2403:18c0:1001:179::1";
+  #   };
+  # };
 
   services.vnstat.enable = true;
   # Traffic Reset Date
