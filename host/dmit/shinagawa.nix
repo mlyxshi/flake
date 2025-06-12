762e931781d9343e3d143a28b4f1d489e6a65ca8
mlyxshi <mlyxdev@gmail.com>
2025-06-12 19:06:06 +0800

    update

--- host/dmit/shinagawa.nix
+++ host/dmit/shinagawa.nix
@@ -12,7 +12,7 @@
   # Prefer IPv4 for DNS resolution
   # networking.getaddrinfo.precedence."::ffff:0:0/96" = 100;
   # Disble IPv6 temporary addresses
-  # boot.kernel.sysctl."net.ipv6.conf.eth0.use_tempaddr" = 0;
+  boot.kernel.sysctl."net.ipv6.conf.eth0.use_tempaddr" = 0;
 
 
   services.openssh.ports = [ 2222 ];
