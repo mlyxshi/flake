1bc927ba435f38432e41658d7671e9facae0842c
mlyxshi <mlyxdev@gmail.com>
2025-07-04 09:12:14 +0800

    update

--- modules/services/komari-agent.nix
+++ modules/services/komari-agent.nix
@@ -6,6 +6,7 @@ in
   systemd.services."komari-agent@" = {
     after = [ "network.target" ];
     serviceConfig = {
+      DynamicUser = true;
       ExecStart = "${package}/bin/komari-agent -e https://top.mlyxshi.com --disable-web-ssh --disable-auto-update -t %i --include-nics eth0";
     };
   };
