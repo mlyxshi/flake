a2d0851cdb95ab7ad1f2075e00c84d60ae166633
mlyxshi <mlyxdev@gmail.com>
2025-07-07 17:47:54 +0800

    update

--- pkgs/snell.nix
+++ pkgs/snell.nix
@@ -10,8 +10,8 @@ stdenvNoCC.mkDerivation rec {
       else "https://dl.nssurge.com/snell/snell-server-v${version}-linux-aarch64.zip";
     hash =
       if stdenvNoCC.hostPlatform.isx86_64
-      then ""
-      else "";
+      then "sha256-DjEfcFRLEhjvuru/hnO93Y3wgu8IcegH6ylcZndV+js="
+      else "sha256-imp36CgZGQeD4eWf+kPep55sM42lHQvwDobfVV9ija0=";
   };
 
   installPhase = ''
