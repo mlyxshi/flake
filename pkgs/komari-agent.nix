fb31876a8d02fa65aae553882d63e0ad37364949
mlyxshi <mlyxdev@gmail.com>
2025-07-20 16:05:16 +0800

    update

--- pkgs/komari-agent.nix
+++ pkgs/komari-agent.nix
@@ -2,18 +2,16 @@
 
 buildGoModule rec {
   pname = "komari-agent";
-  version = "0.0.24";
+  version = "0.1.0";
 
   src = fetchFromGitHub {
     owner = "komari-monitor";
     repo = "komari-agent";
     rev = version;
-    hash = "sha256-zgzG73cTHxa17T20cOPgyoGqMgQZm13g8FmYjDqbI6E=";
+    hash = "";
   };
 
-  # src = ../source/komari-agent;
-
-  vendorHash = "sha256-2tCYq7+r2VCQTULEt7gkK3ocz3YSoKi9yZ7NCVUTiH8=";
+  vendorHash = "";
 
   ldflags = [ "-s" "-w" "-X github.com/komari-monitor/komari-agent/update.CurrentVersion=${version}" ];
 
