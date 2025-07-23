bd92afc177e7163b1d75fe8f0b35188d5b5b90c9
mlyxshi <mlyxdev@gmail.com>
2025-07-23 17:46:22 +0800

    update

--- host/gcp/gcp-hk.nix
+++ host/gcp/gcp-hk.nix
@@ -16,17 +16,40 @@
   services.sing-box.settings = {
     log.level = "info";
     dns = { };
-    endpoints = [ ];
+    endpoints = [
+      {
+        type = "wireguard";
+        tag = "wg-endpoint";
+        address = [ "172.16.0.2/32" ];
+        private_key = { _secret = "/secret/warp-allowed"; };
+        listen_port = 10000;
+
+        peers = [
+          {
+            address = "engage.cloudflareclient.com";
+            port = 2408;
+            public_key = "bmXOC+F1FxEMF9dyiK2H5/1SUtzH0JuVo51h2wPfgyo=";
+            allowed_ips = [ "0.0.0.0/0" ];
+          }
+        ];
+      }
+    ];
     inbounds = [
       {
         type = "shadowsocks";
-        tag = "ss-in";
+        tag = "ss-in-9999";
         listen = "0.0.0.0";
         listen_port = 9999;
         method = "aes-128-gcm";
-        password = {
-          _secret = "/secret/ss-password";
-        };
+        password = { _secret = "/secret/ss-password"; };
+      }
+      {
+        type = "shadowsocks";
+        tag = "ss-in-9998";
+        listen = "0.0.0.0";
+        listen_port = 9998;
+        method = "aes-128-gcm";
+        password = { _secret = "/secret/ss-password"; };
       }
     ];
 
@@ -37,7 +60,14 @@
       }
     ];
 
-    route = { };
+    route = {
+      rules = [
+        {
+          inbound = "ss-in-9998";
+          outbound = "wg-endpoint";
+        }
+      ];
+    };
   };
 
 
