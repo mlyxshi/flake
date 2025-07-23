73535a071274517ed3f02cfcd850df40671f1e15
mlyxshi <mlyxdev@gmail.com>
2025-07-23 18:33:30 +0800

    update

--- host/oracle/jp1.nix
+++ host/oracle/jp1.nix
@@ -7,6 +7,20 @@
     "net.ipv4.ip_forward" = 1;
   };
 
+  services.sing-box.enable = true;
+  services.sing-box.settings = {
+    inbounds = [
+      {
+        type = "shadowsocks";
+        tag = "ss-in-9999";
+        listen = "0.0.0.0";
+        listen_port = 9999;
+        method = "aes-128-gcm";
+        password = { _secret = "/secret/ss-password"; };
+      }
+    ];
+  };
+
   systemd.services."komari-agent@llIhN2egiHfMivbc".overrideStrategy = "asDropin";
   systemd.services."komari-agent@llIhN2egiHfMivbc".wantedBy = [ "multi-user.target" ];
 }
