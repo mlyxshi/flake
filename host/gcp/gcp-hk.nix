6b5fd2aeb8afcdd2fa312c86568175f5818ca66f
mlyxshi <mlyxdev@gmail.com>
2025-07-23 16:15:35 +0800

    update

--- host/gcp/gcp-hk.nix
+++ host/gcp/gcp-hk.nix
@@ -3,19 +3,13 @@
   #   self.nixosModules.containers.podman
   # ];
 
-  # virtualisation.oci-containers.containers.whmcs = {
-  #   image = "docker.io/vpslog/vps-stock-monitor";
-  #   volumes = [ "/var/lib/whmcs:/app/data" ];
-  #   ports = [ "5000:5000" ];
-  #   extraOptions = lib.concatMap (x: [ "--label" x ]) [
-  #     "io.containers.autoupdate=registry"
-  #   ];
-  # };
-
-
 
   # networking.nftables.enable = true;
   # networking.nftables.ruleset = ''
   # '';  
+
+  environment.systemPackages = with pkgs; [
+    sing-box
+  ];
 
 }
