4fbc190e561517f9f0a49fc0356ba1dc31fec34a
mlyxshi <mlyxdev@gmail.com>
2025-07-24 13:47:01 +0800

    update

--- host/oracle/us.nix
+++ host/oracle/us.nix
@@ -55,12 +55,13 @@
 
   services.sing-box.package = pkgs.sing-box.overrideAttrs (previousAttrs: {
     pname = previousAttrs.pname + "-beta";
-    version = "66a767d083fd37b3cd071466636e645bfc96bc96";
-
-    src = previousAttrs.src.override {
+    version = "2.12";
+    src = fetchFromGitHub {
+      owner = "SagerNet";
+      repo = "sing-box";
+      rev = "66a767d083fd37b3cd071466636e645bfc96bc96";
       hash = "";
     };
-
     vendorHash = "";
   });
 
