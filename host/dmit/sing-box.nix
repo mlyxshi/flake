b48ea53344060adfd0ae8f4c8905d0f5e55fce73
mlyxshi <mlyxdev@gmail.com>
2025-07-26 00:49:07 +0800

    update

--- host/dmit/sing-box.nix
+++ host/dmit/sing-box.nix
@@ -2,22 +2,56 @@
   services.sing-box.enable = true;
   services.sing-box.settings = {
     log.level = "info";
+    endpoints = [
+      {
+        type = "wireguard";
+        tag = "wg-endpoint";
+        address = [ "172.16.0.2/32" ];
+        private_key = { _secret = "/secret/warp-allowed"; };
+        listen_port = 10000;
+        peers = [
+          {
+            address = "engage.cloudflareclient.com";
+            port = 2408;
+            public_key = "bmXOC+F1FxEMF9dyiK2H5/1SUtzH0JuVo51h2wPfgyo=";
+            allowed_ips = [ "0.0.0.0/0" ];
+            reserved = [ 129 120 123 ];
+          }
+        ];
+      }
+    ];
     inbounds = [
       {
         type = "shadowsocks";
-        tag = "ss-in";
+        tag = "ss-in-share";
         listen = "0.0.0.0";
         listen_port = 9999;
         managed = true;
         method = "2022-blake3-aes-128-gcm";
         password = { _secret = "/secret/ss-password-2022"; };
       }
+      {
+        type = "shadowsocks";
+        tag = "ss-in-me";
+        listen = "0.0.0.0";
+        listen_port = 8888;
+        method = "aes-128-gcm";
+        password = { _secret = "/secret/ss-password"; };
+      }
     ];
+    route = {
+      rules = [
+        {
+          inbound = "ss-in-me";
+          outbound = "wg-endpoint";
+        }
+      ];
+    };
     services = [
       {
         type = "ssm-api";
         servers = {
-          "/" = "ss-in";
+          "/" = "ss-in-share";
         };
         cache_path = "cache.json";
         listen = "0.0.0.0";
