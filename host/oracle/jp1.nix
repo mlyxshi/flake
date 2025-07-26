2804f5335db2ee7178ac556131fa96fbf5177a6e
mlyxshi <mlyxdev@gmail.com>
2025-07-26 09:50:08 +0800

    update

--- host/oracle/jp1.nix
+++ host/oracle/jp1.nix
@@ -13,10 +13,11 @@
     inbounds = [
       {
         type = "shadowsocks";
+        tag = "ss-in";
         listen = "0.0.0.0";
         listen_port = 8888;
-        method = "aes-128-gcm";
-        password = { _secret = "/secret/ss-password"; };
+        method = "2022-blake3-aes-128-gcm";
+        password = { _secret = "/secret/ss-password-2022"; };
       }
     ];
   };
