0a2db0480203ab0c65e5ee0aa41c633c61a7d257
mlyxshi <mlyxdev@gmail.com>
2025-07-26 12:08:11 +0800

    update

--- host/dmit/sing-box.nix
+++ host/dmit/sing-box.nix
@@ -77,36 +77,24 @@ let
 
 in
 {
-  users = {
-    users.sing-box = {
-      group = "sing-box";
-      isSystemUser = true;
-    };
-    groups.sing-box = { };
-  };
-
   systemd.services.sing-box-share = {
+    after = [ "network-online.target" ];
     preStart = utils.genJqSecretsReplacementSnippet config-share "/run/sing-box/config.json";
     serviceConfig = {
       StateDirectory = "sing-box";
       RuntimeDirectory = "sing-box";
-      ExecStart = [
-        ""
-        "${lib.getExe sing-box-beta} -D \${STATE_DIRECTORY} -C \${RUNTIME_DIRECTORY} run"
-      ];
+      ExecStart = "${lib.getExe sing-box-beta} -D \${STATE_DIRECTORY} -C \${RUNTIME_DIRECTORY} run";
     };
     wantedBy = [ "multi-user.target" ];
   };
 
   systemd.services.sing-box-my = {
+    after = [ "network-online.target" ];
     preStart = utils.genJqSecretsReplacementSnippet config-my "/run/sing-box/config-my.json";
     serviceConfig = {
       StateDirectory = "sing-box-my";
       RuntimeDirectory = "sing-box-my";
-      ExecStart = [
-        ""
-        "${lib.getExe sing-box-beta} -D \${STATE_DIRECTORY} -C \${RUNTIME_DIRECTORY} run"
-      ];
+      ExecStart = "${lib.getExe sing-box-beta} -D \${STATE_DIRECTORY} -C \${RUNTIME_DIRECTORY} run";
     };
     wantedBy = [ "multi-user.target" ];
   };
