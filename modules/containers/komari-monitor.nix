e89fbe9c8bd1e688477080473310a48f8b8a4491
mlyxshi <mlyxdev@gmail.com>
2025-08-14 06:40:06 +0800

    update

--- modules/containers/komari-monitor.nix
+++ modules/containers/komari-monitor.nix
@@ -1,7 +1,7 @@
 { config, pkgs, lib, ... }: {
 
   virtualisation.oci-containers.containers.komari-monitor = {
-    image = "ghcr.io/komari-monitor/komari:main";
+    image = "ghcr.io/komari-monitor/komari:latest";
     volumes = [ "/var/lib/komari-monitor:/app/data" ];
     extraOptions = lib.concatMap (x: [ "--label" x ]) [
       "io.containers.autoupdate=registry"
