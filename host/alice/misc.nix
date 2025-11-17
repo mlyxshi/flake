4b2bea2db4459012827e4932d2b348f3c1cf7270
mlyxshi <mlyxdev@gmail.com>
2025-11-17 13:16:56 +0800

    update

--- host/alice/misc.nix
+++ host/alice/misc.nix
@@ -17,4 +17,33 @@
     AGENT_MONTH_ROTATE = "24";
     AGENT_INCLUDE_MOUNTPOINTS = "/";
   };
+
+  services.sing-box.enable = true;
+  services.sing-box.settings = {
+    log.level = "info";
+
+    inbounds = [
+      {
+        type = "shadowsocks";
+        tag = "ss-in-basic";
+        listen = "0.0.0.0";
+        listen_port = 80;
+        network = "tcp";
+        method = "2022-blake3-aes-128-gcm";
+        password = { _secret = "/secret/ss-password-2022"; };
+      }
+    ];
+
+    outbounds = [
+      {
+        type = "socks";
+        tag = "TW";
+        server = "2a14:67c0:116::1";
+        server_port = 10001;
+        version = "5";
+        username = "alice";
+        password = "alicefofo123..OVO";
+      }
+    ];
+  };
 }
