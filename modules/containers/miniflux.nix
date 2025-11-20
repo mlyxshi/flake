31bda6cedc29bb212525cd0a340a3cf06a6d001a
mlyxshi <mlyxdev@gmail.com>
2025-11-20 09:19:54 +0800

    update

--- modules/containers/miniflux.nix
+++ modules/containers/miniflux.nix
@@ -20,7 +20,7 @@
       RUN_MIGRATIONS = "1";
       SCHEDULER_ROUND_ROBIN_MIN_INTERVAL = "10";
       POLLING_FREQUENCY = "10";
-      POLLING_PARSING_ERROR_LIMIT = "3";
+      POLLING_PARSING_ERROR_LIMIT = "0";
       METRICS_COLLECTOR = "1";
       METRICS_ALLOWED_NETWORKS = "0.0.0.0/0";
       DATABASE_URL = "postgres://postgres:postgres@miniflux-postgres/miniflux?sslmode=disable";
