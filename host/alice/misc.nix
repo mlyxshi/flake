98ccec75bc1ef61ea0f158130a7a80cd8cc04a18
mlyxshi <mlyxdev@gmail.com>
2025-11-21 19:03:00 +0800

    update

--- host/alice/misc.nix
+++ host/alice/misc.nix
@@ -60,4 +60,13 @@
   environment.systemPackages = with pkgs; [
     hath-rust
   ];
+
+  systemd.services.hath = {
+    serviceConfig.ExecStart = "${pkgs.hath-rust}/bin/hath-rust";
+    serviceConfig.StateDirectory = "hath";
+    serviceConfig.WorkingDirectory = "%S/hath";
+    wants = [ "network-online.target" ];
+    after = [ "network-online.target" ];
+    wantedBy = [ "multi-user.target" ];
+  };
 }
