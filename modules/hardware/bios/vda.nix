cd0f9ce673ca637bf3b1ca3408c45f4a45c430b0
mlyxshi <mlyxdev@gmail.com>
2025-11-22 16:13:28 +0800

    update

--- modules/hardware/bios/vda.nix
+++ modules/hardware/bios/vda.nix
@@ -1,4 +1,4 @@
-{ modulesPath, pkgs, lib, ... }: {
+{ modulesPath, pkgs, lib, config, ... }: {
   imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
 
   boot.initrd.systemd.enable = true;
@@ -8,7 +8,7 @@
   boot.loader.limine.efiSupport = false;
   boot.loader.limine.biosDevice = "/dev/vda";
   boot.loader.limine.maxGenerations = 2;
-  boot.loader.timeout = 2; 
+  boot.loader.timeout = 2;
 
   fileSystems."/boot" = {
     device = "/dev/vda1";
@@ -18,5 +18,17 @@
   fileSystems."/" = {
     device = "/dev/vda2";
     fsType = "ext4";
+    autoResize = true; # resizes filesystem to occupy whole partition
+  };
+
+  boot.growPartition = true; # resizes partition to occupy whole disk
+
+  system.build.raw = import "${pkgs.path}/nixos/lib/make-disk-image.nix" {
+    inherit config lib pkgs;
+    format = "raw";
+    copyChannel = false;
+    partitionTableType = "legacy+boot"; # limine bootloader
+    bootSize = "128M";
+    additionalSpace = "128M";
   };
 }
