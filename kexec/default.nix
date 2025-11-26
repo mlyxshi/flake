d2eac26e47b9933d9383f4d2a9552e7fff1c7479
mlyxshi <mlyxdev@gmail.com>
2025-11-26 13:54:51 +0800

    update

--- kexec/default.nix
+++ kexec/default.nix
@@ -13,13 +13,13 @@
   boot.initrd.network.ssh.enable = true;
   boot.initrd.systemd.services.sshd.preStart = lib.mkForce "/bin/chmod 0600 /etc/ssh/ssh_host_ed25519_key";
 
-  # qemu + ext4 + vfat + efivarfs + overlay
+  # qemu + ext4 + vfat + efivarfs
   # add extra kernel modules: https://github.com/NixOS/nixpkgs/blob/master/nixos/modules/profiles/all-hardware.nix
   boot.initrd.kernelModules = [ "virtio_net" "virtio_pci" "virtio_mmio" "virtio_blk" "virtio_scsi" "virtio_balloon" "virtio_console" "9p" "9pnet_virtio" ]
     ++ [ "ext4" ]
     ++ [ "vfat" "nls_cp437" "nls_iso8859-1" ]
-    ++ [ "efivarfs" ]
-    ++ [ "erofs" "overlay" ];
+    ++ [ "efivarfs" ];
+
 
   boot.initrd.systemd.contents = {
     "/etc/ssl/certs/ca-certificates.crt".source = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
@@ -35,12 +35,6 @@
       AAAEDIN2VWFyggtoSPXcAFy8dtG1uAig8sCuyE21eMDt2GgJBWcxb/Blaqt1auOtE+F8QU
       WrUotiC5qBJ+UuEWdVCbAAAACnJvb3RAbml4b3MBAgM=
       -----END OPENSSH PRIVATE KEY-----
-    '';
-
-    "/etc/nix/nix.conf".text = ''
-      extra-experimental-features = nix-command flakes
-      substituters = https://cache.nixos.org
-      trusted-public-keys = cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=
     '';
 
     "/etc/yazi/yazi.toml".text = ''
@@ -61,36 +55,14 @@
     "/root/.terminfo".source = ./.terminfo; # macos default terminal is xterm-256color
   };
 
-  boot.initrd.systemd.storePaths = [
-    "${pkgs.file}/share/misc/magic.mgc" # file dependency 
-  ];
-
   boot.initrd.systemd.extraBin = {
-    # nix
-    nix = "${pkgs.nix}/bin/nix";
-    nix-store = "${pkgs.nix}/bin/nix-store";
-    nix-env = "${pkgs.nix}/bin/nix-env";
-    busybox = "${pkgs.busybox-sandbox-shell}/bin/busybox";
-    nixos-enter = "${pkgs.nixos-install-tools}/bin/nixos-enter";
-    unshare = "${pkgs.util-linux}/bin/unshare";
-
-    # net
     ip = "${pkgs.iproute2}/bin/ip";
     curl = "${pkgs.curl}/bin/curl";
-
-    # fs
-    "mkfs.fat" = "${pkgs.dosfstools}/bin/mkfs.fat";
-    "mkfs.ext4" = "${pkgs.e2fsprogs}/sbin/mkfs.ext4";
-    sgdisk = "${pkgs.gptfdisk}/bin/sgdisk"; # GPT
-    parted = "${pkgs.parted}/bin/parted"; # MBR
-    file = "${pkgs.file}/bin/file";
     lsblk = "${pkgs.util-linux}/bin/lsblk";
-
     # debug
     htop = "${pkgs.htop}/bin/htop";
     yazi = "${pkgs.yazi-unwrapped}/bin/yazi";
     hx = "${pkgs.helix}/bin/hx";
-    # strace = "${pkgs.strace}/bin/strace";
   };
 
   boot.initrd.systemd.emergencyAccess = true;
