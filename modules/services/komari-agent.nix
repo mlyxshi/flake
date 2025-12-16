8e5e4d0d70cfcc220fc46d28a8604c9db0f405c6
mlyxshi <mlyxdev@gmail.com>
2025-12-16 09:48:23 +0800

    update

--- modules/services/komari-agent.nix
+++ modules/services/komari-agent.nix
@@ -12,7 +12,7 @@
       AGENT_DISABLE_AUTO_UPDATE = "true";
       AGENT_DISABLE_WEB_SSH = "true";
       AGENT_MONTH_ROTATE = lib.mkDefault "1";
-      AGENT_CONFIG_FILE = "/secret/komari"; # token
+      AGENT_CONFIG_FILE = "/secret/komari"; # auto_discovery_key
     };
     serviceConfig.ExecStart = lib.mkDefault "${pkgs.komari-agent}/bin/komari-agent";
     serviceConfig.DynamicUser = true;
