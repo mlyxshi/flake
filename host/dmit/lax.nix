8e3d3f8ba1ad27fa952ae7879bd2af9228530187
mlyxshi <mlyxdev@gmail.com>
2025-12-16 17:03:50 +0800

    update

--- host/dmit/lax.nix
+++ host/dmit/lax.nix
@@ -12,8 +12,6 @@
   boot.initrd.systemd.enable = true;
   boot.initrd.systemd.emergencyAccess = true;
 
-  boot.loader.grub.device = "nodev";
-
   fileSystems."/old-root" = {
     device = "/dev/vda1";
     fsType = "ext4";
@@ -28,5 +26,39 @@
     fsType = "ext4";
     options = [ "X-mount.subdir=nix" ];
   };
+
+  environment.systemPackages = [
+    (writeShellScriptBin "update" ''
+      if [[ -e "/flake/flake.nix" ]]
+      then
+        cd /flake
+        git pull   
+      else
+        git clone --depth=1  git@github.com:mlyxshi/flake /flake
+        cd /flake
+      fi  
+
+
+      SYSTEM=$(nix build --no-link --print-out-paths .#nixosConfigurations.$HOST.config.system.build.toplevel)
+
+      if [ -n "$SYSTEM" ]
+      then
+        [[ -e "/run/current-system" ]] && nix store diff-closures /run/current-system $SYSTEM
+        cat <<END > /boot/grub/custom.cfg
+        menuentry "NixOS" --id NixOS {
+          insmod ext2
+          search -f /etc/hostname --set root
+          linux $SYSTEM/kernel root=fstab $(cat $SYSTEM/kernel-params) init=$SYSTEM/init
+          initrd $SYSTEM/initrd
+        }
+        set default="NixOS"
+        END
+      else
+        echo "Build Failed"
+        exit 1
+      fi
+    '')
+
+  ];
 
 }
