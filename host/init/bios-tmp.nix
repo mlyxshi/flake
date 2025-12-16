1402ef502e8e2167db793230e1b702edd3c73292
mlyxshi <mlyxdev@gmail.com>
2025-12-17 01:55:46 +0800

    update

--- host/init/bios-tmp.nix
+++ host/init/bios-tmp.nix
@@ -28,7 +28,7 @@
 
   systemd.network.networks.ethernet-static = {
     matchConfig.Name = "en*";
-    networkConfig.Address="154.17.19.228/32";
+    networkConfig.Address = "154.17.19.228/32";
     routes = [
       {
         Gateway = "193.41.250.250";
@@ -36,28 +36,36 @@
       }
     ];
   };
-  
+
   networking.firewall.enable = false;
 
   boot.initrd.systemd.enable = true;
   boot.initrd.systemd.emergencyAccess = true;
 
-  boot.loader.limine.enable = true;
-  boot.loader.limine.biosSupport = true;
-  boot.loader.limine.efiSupport = false;
-  boot.loader.limine.biosDevice = "/dev/vda";
-  boot.loader.limine.maxGenerations = 2;
-  boot.loader.timeout = 3; # inmediate boot
+  # boot.loader.limine.enable = true;
+  # boot.loader.limine.biosSupport = true;
+  # boot.loader.limine.efiSupport = false;
+  # boot.loader.limine.biosDevice = "/dev/vda";
+  # boot.loader.limine.maxGenerations = 2;
+  # boot.loader.timeout = 3; # inmediate boot
 
-  fileSystems."/boot" = {
-    device = "/dev/vda1";
-    fsType = "vfat";
-  };
+  # fileSystems."/boot" = {
+  #   device = "/dev/vda1";
+  #   fsType = "vfat";
+  # };
+
+  # fileSystems."/" = {
+  #   device = "/dev/vda2";
+  #   fsType = "ext4";
+  # };
+
+  boot.loader.grub.device = "/dev/vda";
 
   fileSystems."/" = {
-    device = "/dev/vda2";
+    device = "/dev/vda1";
     fsType = "ext4";
   };
+
 
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMpaY3LyCW4HHqbp4SA4tnA+1Bkgwrtro2s/DEsBcPDe"
@@ -93,11 +101,12 @@
     inherit config lib pkgs;
     format = "raw";
     copyChannel = false;
-    partitionTableType = "legacy+boot"; # limine bootloader
+    # partitionTableType = "legacy+boot"; # limine bootloader
+    partitionTableType = "legacy"; # grub bootloader
     bootSize = "128M";
     # additionalSpace = "128M";
     # diskSize = 10240; # 10G
-    diskSize = 20480; # 20G
+    # diskSize = 20480; # 20G
     baseName = config.networking.hostName;
   };
 }
