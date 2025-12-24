c4d6b69ccadd1f0159f2585e3cb1f5749f0b19aa
mlyxshi <mlyxdev@gmail.com>
2025-12-24 20:05:31 +0800

    update

---
+++ kexec/cloud-init-example/a.sh
@@ -0,0 +1,77 @@
+# CLOUD_INIT_CONF="./version1.yml"
+# CLOUD_INIT_CONF="./version2.yml"
+CLOUD_INIT_CONF="./version2-ipv6.yml"
+
+NETWORKD_CONF="./test"
+VERSION=$(yq .version $CLOUD_INIT_CONF)
+
+if [ "$VERSION" = "1" ]; then
+  IP=$(yq .config[0].subnets[0].address $CLOUD_INIT_CONF)
+  NETMASK=$(yq .config[0].subnets[0].netmask $CLOUD_INIT_CONF)
+  GATEWAY=$(yq .config[0].subnets[0].gateway $CLOUD_INIT_CONF)
+
+  if [ "$NETMASK" = "255.255.255.255" ]; then
+    CIDR=32
+  elif [ "$NETMASK" = "255.255.255.0" ]; then
+    CIDR=24
+  else
+    echo "Unsupported netmask: $NETMASK" >&2
+    exit 1
+  fi
+
+  {
+    echo "[Match]"
+    echo "Name=en*"
+    echo
+    echo "[Network]"
+    echo "Address=$IP/$CIDR"
+    echo
+    echo "[Route]"
+    echo "Gateway=$GATEWAY"
+    if [ "$CIDR" -eq 32 ]; then
+      echo "GatewayOnLink=yes"
+    fi
+  } > $NETWORKD_CONF
+
+elif [ "$VERSION" = "2" ]; then
+  IP=$(yq .ethernets.eth0.addresses[0] $CLOUD_INIT_CONF)
+  GATEWAY4=$(yq .ethernets.eth0.gateway4 $CLOUD_INIT_CONF)
+  
+  if [ "$GATEWAY4" = "null" ]; then
+    echo "IPV6 Only"
+    GATEWAY6=$(yq '.ethernets.eth0.gateway6' $CLOUD_INIT_CONF)
+    {
+      echo "[Match]"
+      echo "Name=en*"
+      echo
+      echo "[Network]"
+      echo "Address=${IP%%/*}/128"
+      echo
+      echo "[Route]"
+      echo "Gateway=$GATEWAY6"
+      echo "GatewayOnLink=yes"
+    } > $NETWORKD_CONF
+  else
+    echo "Use IPV4"
+    {
+      echo "[Match]"
+      echo "Name=en*"
+      echo
+      echo "[Network]"
+      echo "Address=$IP"
+      echo
+      echo "[Route]"
+      echo "Gateway=$GATEWAY4"
+    } > $NETWORKD_CONF
+    case "$IP" in
+      */32)
+        echo "CIDR is /32"
+        echo "GatewayOnLink=yes" >> $NETWORKD_CONF
+        ;;
+      *)
+        echo "CIDR is not /32"
+        ;;
+    esac
+
+  fi
+fi
