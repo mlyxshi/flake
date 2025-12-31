0a768e7e8412a33977e09cc2bc4acd321ae99ee2
mlyxshi <mlyxdev@gmail.com>
2025-12-31 20:56:38 +0800

    update

--- modules/hardware/bios/limine.nix
+++ modules/hardware/bios/limine.nix
@@ -14,7 +14,7 @@
   boot.loader.limine.enable = true;
   boot.loader.limine.biosSupport = true;
   boot.loader.limine.efiSupport = false;
-  boot.loader.limine.biosDevice = "/dev/vda";
+  boot.loader.limine.biosDevice = lib.mkDefault "/dev/vda";
   boot.loader.limine.partitionIndex = 1; # GPT bios boot partition
   boot.loader.limine.maxGenerations = 2;
   boot.loader.timeout = 1;
