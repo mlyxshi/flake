ed36522483452f794957c1d95da00f9ccdd416be
mlyxshi <mlyxdev@gmail.com>
2026-01-01 10:02:48 +0800

    update

--- host/init/bios.nix
+++ host/init/bios.nix
@@ -30,11 +30,10 @@
   boot.initrd.systemd.emergencyAccess = true;
 
   boot.loader.limine.enable = true;
-  # boot.loader.limine.force = true;
   boot.loader.limine.biosSupport = true;
   boot.loader.limine.efiSupport = false;
   boot.loader.limine.biosDevice = "/dev/vda";
-  boot.loader.limine.partitionIndex = 1; # GPT bios boot partition
+  # boot.loader.limine.partitionIndex = 1; # GPT bios boot partition
   boot.loader.limine.maxGenerations = 2;
   boot.loader.timeout = 1;
 
