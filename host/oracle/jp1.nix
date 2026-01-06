2edefdcfb837ba9f3ec20b869e337f3d582077df
mlyxshi <mlyxdev@gmail.com>
2026-01-06 10:53:46 +0800

    update

--- host/oracle/jp1.nix
+++ host/oracle/jp1.nix
@@ -9,28 +9,32 @@
   imports = [
     self.nixosModules.containers.podman
     self.nixosModules.containers.pocket-id
-    self.nixosModules.services.snell
+    # self.nixosModules.services.snell
   ];
 
-  # services.sing-box.enable = true;
-  # services.sing-box.settings = {
-  #   log.level = "info";
-  #   inbounds = [
-  #     {
-  #       type = "shadowsocks";
-  #       tag = "ss-in";
-  #       listen = "0.0.0.0";
-  #       listen_port = 8888;
-  #       network = "tcp";
-  #       method = "2022-blake3-aes-128-gcm";
-  #       password = {
-  #         _secret = "/secret/ss-password-2022";
-  #       };
-  #       multiplex = {
-  #         enabled = true;
-  #       };
-  #     }
-  #   ];
-  # };
+  services.sing-box.enable = true;
+  services.sing-box.settings = {
+    log.level = "info";
+    inbounds = [
+      {
+        type = "anytls";
+        tag = "anytls-in";
+        listen = "0.0.0.0";
+        listen_port = 9999;
+        users = [
+          {
+            password = {
+              _secret = "/secret/ss-password-2022";
+            };
+          }
+        ];
+        tls = {
+          enabled = true;
+          certificate_path = "/root/certificate";
+          key_path = "/root/key";
+        };
+      }
+    ];
+  };
 
 }
