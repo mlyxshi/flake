840a79c84b7b08a9e7df0de5724cb10d448511b3
mlyxshi <mlyxdev@gmail.com>
2026-01-08 19:10:22 +0800

    update

--- modules/services/snell.nix
+++ modules/services/snell.nix
@@ -6,13 +6,6 @@
   ...
 }:
 {
-
-  nixpkgs.overlays = [
-    (final: prev: {
-      snell = prev.callPackage (self + "/pkgs/snell.nix") { };
-    })
-  ];
-
   systemd.services.snell = {
     after = [ "network.target" ];
     serviceConfig.ExecStart = "${pkgs.snell}/bin/snell-server -c /secret/snell";
