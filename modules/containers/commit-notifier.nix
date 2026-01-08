5f5fecdef952a9de58e42ba3f1e03ca4f799f9ee
mlyxshi <mlyxdev@gmail.com>
2026-01-08 15:00:49 +0800

    update

--- modules/containers/commit-notifier.nix
+++ modules/containers/commit-notifier.nix
@@ -7,7 +7,11 @@
 {
   virtualisation.oci-containers.containers.commit-notifier = {
     image = "ghcr.io/linyinfeng/commit-notifier";
-    volumes = [ "/var/lib/commit-notifier:/data ];
+    volumes = [ "/var/lib/commit-notifier:/data" ];
     environmentFiles = [ /secret/commit-notifier ];
+    extraOptions = [
+      "--label"
+      "io.containers.autoupdate=registry"
+    ];
   };
 }
