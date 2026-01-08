40ff9ffccfef31b7c3464915d3feee001102c42c
mlyxshi <mlyxdev@gmail.com>
2026-01-08 14:59:59 +0800

    update

--- modules/containers/commit-notifier.nix
+++ modules/containers/commit-notifier.nix
@@ -9,6 +9,5 @@
     image = "ghcr.io/linyinfeng/commit-notifier";
     volumes = [ "/var/lib/commit-notifier:/data ];
     environmentFiles = [ /secret/commit-notifier ];
-    extraOptions =[ "--label" "io.containers.autoupdate=registry" ];
   };
 }
