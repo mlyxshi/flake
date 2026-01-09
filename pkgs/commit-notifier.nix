95d0b062f15ec42473028e44eae59f8f09a89e42
mlyxshi <mlyxdev@gmail.com>
2026-01-09 09:51:09 +0800

    update

--- pkgs/commit-notifier.nix
+++ pkgs/commit-notifier.nix
@@ -1,27 +1,36 @@
 {
-  lib,
   rustPlatform,
+  pkg-config,
+  openssl,
+  sqlite,
+  zlib,
+  libgit2,
   fetchFromGitHub,
 }:
 
 rustPlatform.buildRustPackage rec {
   pname = "commit-notifier";
-  version = "unstable-2026-01-03";
-
-  src = fetchFromGitHub {
+  version = "0-unstable-2026-01-03";
+  src = fetchFromGitHub ({
     owner = "linyinfeng";
     repo = "commit-notifier";
     rev = "21c6617645d1bdb0ce213f14053665e3474756bd";
-    hash = "sha256-iKdqHMI+TJNbxxmFC/SP6DMCh1EiTWAnrXJ0Q6iSK6g=";
-  };
+    fetchSubmodules = true;
+    sha256 = "sha256-iKdqHMI+TJNbxxmFC/SP6DMCh1EiTWAnrXJ0Q6iSK6g=";
+  });
 
   cargoHash = "sha256-OTXNRVEHT8cor76mNTbstb0GZBPal9qlie1wz2W5gpA=";
 
-  meta = {
-    description = "A simple bot tracking git commits/PRs/issues/branches";
-    homepage = "https://github.com/linyinfeng/commit-notifier";
-    license = lib.licenses.mit;
-    maintainers = with lib.maintainers; [ ];
-    mainProgram = "commit-notifier";
-  };
+  RUSTC_BOOTSTRAP = 1;
+
+  buildInputs = [
+    openssl
+    sqlite
+    libgit2
+    zlib
+  ];
+
+  # TODO libssh2-sys failed to pass test
+  doCheck = false;
+  nativeBuildInputs = [ pkg-config ];
 }
