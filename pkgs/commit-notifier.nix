926e265c39079f6bef589523058f8f6a3fe1808b
mlyxshi <mlyxdev@gmail.com>
2026-01-09 12:05:07 +0800

    update

--- pkgs/commit-notifier.nix
+++ pkgs/commit-notifier.nix
@@ -11,12 +11,12 @@
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "commit-notifier";
   version = "0-unstable-2026-01-03";
-  src = fetchFromGitHub ({
+  src = fetchFromGitHub {
     owner = "linyinfeng";
     repo = "commit-notifier";
     rev = "21c6617645d1bdb0ce213f14053665e3474756bd";
     sha256 = "sha256-iKdqHMI+TJNbxxmFC/SP6DMCh1EiTWAnrXJ0Q6iSK6g=";
-  });
+  };
 
   cargoHash = "sha256-OTXNRVEHT8cor76mNTbstb0GZBPal9qlie1wz2W5gpA=";
 
