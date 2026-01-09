42dedb8d79b598152f42c05fd6745828556f5e11
mlyxshi <mlyxdev@gmail.com>
2026-01-09 11:47:06 +0800

    update

--- modules/services/commit-notifier.nix
+++ modules/services/commit-notifier.nix
@@ -13,9 +13,10 @@
     })
   ];
 
-  # mkdir -p /data/chats/696869490
+  # mkdir -p /var/lib/commit-notifier/chats/696869490
   # /notifier repo-add nixpkgs https://github.com/NixOS/nixpkgs
   # /notifier repo-edit nixpkgs --branch-regex master|nixos-unstable|nixos-unstable-small
+  # /notifier condition-remove nixpkgs in-master
   # /notifier condition-add —type remove-if-in-branch —expr nixos-unstable nixpkgs in-nixos-unstable
   # /notifier pr-add https://github.com/NixOS/nixpkgs/pull/476546
 
