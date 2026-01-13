dfce23e88238f8dc31f8cc7aca405aac6e43cf14
mlyxshi <mlyxdev@gmail.com>
2026-01-13 12:29:44 +0800

    update

--- host/dmit/misc.nix
+++ host/dmit/misc.nix
@@ -23,36 +23,36 @@
         ip protocol icmp accept
         ip6 nexthdr icmpv6 accept
         ct state {established, related} accept
-        tcp dport { 23333, 8888, 9999 } accept
-        udp dport { 9999 } accept
+        tcp dport { 23333, 8888, 5201 } accept
+        udp dport { 5201 } accept
       }
     }
   '';
 
-  services.sing-box.enable = true;
-  services.sing-box.settings = {
-    log.level = "info";
-    inbounds = [
-      {
-        type = "anytls";
-        tag = "anytls-in";
-        listen = "0.0.0.0";
-        listen_port = 9999;
-        users = [
-          {
-            password = {
-              _secret = "/secret/ss-password-2022";
-            };
-          }
-        ];
-        tls = {
-          enabled = true;
-          certificate_path = "/secret/self-sign-certificate";
-          key_path = "/secret/self-sign-key";
-        };
-      }
-    ];
-  };
+  # services.sing-box.enable = true;
+  # services.sing-box.settings = {
+  #   log.level = "info";
+  #   inbounds = [
+  #     {
+  #       type = "anytls";
+  #       tag = "anytls-in";
+  #       listen = "0.0.0.0";
+  #       listen_port = 9999;
+  #       users = [
+  #         {
+  #           password = {
+  #             _secret = "/secret/ss-password-2022";
+  #           };
+  #         }
+  #       ];
+  #       tls = {
+  #         enabled = true;
+  #         certificate_path = "/secret/self-sign-certificate";
+  #         key_path = "/secret/self-sign-key";
+  #       };
+  #     }
+  #   ];
+  # };
 
   systemd.services.komari-agent.environment.AGENT_MONTH_ROTATE = "24";
 }
