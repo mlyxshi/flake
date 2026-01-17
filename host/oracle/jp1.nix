1f1bbf196690a1aca1adc6ece698d0f7d93a1719
mlyxshi <mlyxdev@gmail.com>
2026-01-17 19:51:13 +0800

    update

--- host/oracle/jp1.nix
+++ host/oracle/jp1.nix
@@ -9,30 +9,31 @@
   imports = [
     self.nixosModules.containers.podman
     self.nixosModules.containers.pocket-id
+    self.nixosModules.services.snell
   ];
 
-  services.sing-box.enable = true;
-  services.sing-box.settings = {
-    log.level = "info";
-    inbounds = [
-      {
-        type = "anytls";
-        tag = "anytls-in";
-        listen = "0.0.0.0";
-        listen_port = 8888;
-        users = [
-          {
-            password = {
-              _secret = "/secret/proxy-pwd";
-            };
-          }
-        ];
-        tls = {
-          enabled = true;
-          insecure = true;
-        };
-      }
-    ];
-  };
+  # services.sing-box.enable = true;
+  # services.sing-box.settings = {
+  #   log.level = "info";
+  #   inbounds = [
+  #     {
+  #       type = "anytls";
+  #       tag = "anytls-in";
+  #       listen = "0.0.0.0";
+  #       listen_port = 8888;
+  #       users = [
+  #         {
+  #           password = {
+  #             _secret = "/secret/proxy-pwd";
+  #           };
+  #         }
+  #       ];
+  #       tls = {
+  #         enabled = true;
+  #         insecure = true;
+  #       };
+  #     }
+  #   ];
+  # };
 
 }
