b348012d288458c011dd1d8abff9c9267c0bd92d
mlyxshi <mlyxdev@gmail.com>
2026-01-27 17:48:56 +0800

    update

--- pkgs/transmission.nix
+++ pkgs/transmission.nix
@@ -13,9 +13,9 @@ stdenv.mkDerivation (finalAttrs: {
   src = fetchFromGitHub {
     owner = "transmission";
     repo = "transmission";
-    fetchSubmodules = true;
-    rev = "272401184f0736e6063f9da90be7d037e907508a";
+    tag = finalAttrs.version;
     hash = "sha256-glmwa06+jCyL9G2Rc58Yrvzo+/6Qu3bqwqy02RWgG64=";
+    fetchSubmodules = true;
   };
 
   nativeBuildInputs = [
