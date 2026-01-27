a40363559966bfce990e7c4dd227e8f76bdaf88d
mlyxshi <mlyxdev@gmail.com>
2026-01-27 13:27:03 +0800

    update

--- pkgs/transmission.nix
+++ pkgs/transmission.nix
@@ -3,6 +3,7 @@
   stdenv,
   fetchFromGitHub,
   cmake,
+  curl,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
@@ -12,11 +13,16 @@ stdenv.mkDerivation (finalAttrs: {
   src = fetchFromGitHub {
     owner = "transmission";
     repo = "transmission";
+    fetchSubmodules = true;
     rev = "272401184f0736e6063f9da90be7d037e907508a";
-    hash = "";
+    hash = "sha256-glmwa06+jCyL9G2Rc58Yrvzo+/6Qu3bqwqy02RWgG64=";
   };
 
   nativeBuildInputs = [
     cmake
+  ];
+
+  buildInputs = [
+    curl
   ];
 })
