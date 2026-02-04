30df841aab291102e84c4caf1587ef3e612d693e
mlyxshi <mlyxdev@gmail.com>
2026-02-05 01:47:45 +0800

    update

--- modules/services/cloudflare-warp.nix
+++ modules/services/cloudflare-warp.nix
@@ -4,13 +4,9 @@
 }:
 {
 
-  environment.systemPackages = with pkgs; [
-    cloudflare-warp
-  ];
-
   systemd.services.cloudflare-warp-daemon = {
     after = [ "network.target" ];
-    serviceConfig.ExecStart = "${pkgs.cloudflare-warp}/bin/warp-svc";
+    serviceConfig.ExecStart = "${pkgs.cloudflare-warp.override { headless = true; }}/bin/warp-svc";
     serviceConfig.StateDirectory = "cloudflare-warp";
     wantedBy = [ "multi-user.target" ];
   };
