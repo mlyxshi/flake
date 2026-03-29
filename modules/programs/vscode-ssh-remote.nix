6ef823139536cf088e9f2a16193051b4f9eb5488
mlyxshi <mlyxdev@gmail.com>
2026-03-29 15:09:33 +0800

    update

--- modules/programs/vscode-ssh-remote.nix
+++ modules/programs/vscode-ssh-remote.nix
@@ -16,7 +16,8 @@
   # The process of vscode remote server setup
   # See more details: nix build nixpkgs#vscode-extensions.ms-vscode-remote.remote-ssh         
   # share/vscode/extensions/ms-vscode-remote.remote-ssh/out/install-script/scripts/linux-exec-server-installer.sh
-
+  # https://github.com/JasonSloan/DeepFusion/blob/7623b806e37fc83447ff9d10920056ca339ec02e/16.%20IDE/01.%20vscode/11.%20Connect-to-an-offline-server.md
+ 
   # 1. install Visual Studio Code CLI Standalone which is a statically linked binary named like "code-cfbea10c5ffb233ea9177d34726e6056e89913dc" to ~/.vscode-server
   # 2. ~/.vscode-server/code-cfbea10c5ffb233ea9177d34726e6056e89913dc status
   # [2026-03-29 06:38:26] error This machine does not meet Visual Studio Code Server's prerequisites, expected either...
