34bf25805a2bda31c3919716f183f4ca8f792cd7
mlyxshi <mlyxdev@gmail.com>
2026-05-17 22:51:30 +0800

    update

--- host/dmit/default.nix
+++ host/dmit/default.nix
@@ -9,11 +9,11 @@ nixpkgs.lib.nixosSystem {
     self.nixosModules.nixos.server
     self.nixosModules.hardware.bios.limine
     self.nixosModules.network.cloud-init
-    
+
     self.nixosModules.services.komari-agent
     self.nixosModules.services.warp-tor
     self.nixosModules.services.snell
-    self.nixosModules.pr.traefik
+    self.nixosModules.pr.traefik # remove when pr 490985 merged
     ./misc.nix
     {
       nixpkgs.hostPlatform = "x86_64-linux";
--- host/gcp/default.nix
+++ host/gcp/default.nix
@@ -9,6 +9,9 @@ nixpkgs.lib.nixosSystem {
     self.nixosModules.nixos.server
     self.nixosModules.hardware.uefi.gpt-auto
     self.nixosModules.network.dhcp
+
+    self.nixosModules.services.komari-agent
+    self.nixosModules.pr.traefik # remove when pr 490985 merged
     ./misc.nix
     {
       nixpkgs.hostPlatform = "x86_64-linux";
