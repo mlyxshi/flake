e407f67e7f3aa1cdefcbf03c43916d540f6b5f62
mlyxshi <mlyxdev@gmail.com>
2026-05-18 17:48:04 +0800

    update

--- host/gcp/misc.nix
+++ host/gcp/misc.nix
@@ -12,6 +12,9 @@
   #   self.nixosModules.programs.vscode-ssh-remote
   # ];
 
+  boot.kernelParams = [ "console=ttyS0,115200" ];
+  services.getty.autologinUser = "root";
+
   systemd.services.komari-agent.environment.AGENT_ENDPOINT = "http://138.2.16.45";
   systemd.services.komari-agent.environment.AGENT_CUSTOM_IPV4 = "35.212.172.97";
 }
