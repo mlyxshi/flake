01d842e0d6b1f9ff9236d6db94ebd97d700dc75c
mlyxshi <mlyxdev@gmail.com>
2026-06-04 20:27:00 +0800

    update

--- pkgs/module-test.nix
+++ pkgs/module-test.nix
@@ -12,10 +12,45 @@
   options.test = lib.mkOption { type = lib.types.str; };
   options.path = lib.mkOption { type = lib.types.str; };
   options.test2 = lib.mkOption { type = lib.types.str; };
-  options.default = lib.mkOption { type = lib.types.str; default = "default";};
+  options.default = lib.mkOption {
+    type = lib.types.str;
+    default = "default";
+  };
+
+  options.subraw = lib.mkOption {
+    type = lib.types.submodule {
+      options = {
+        foo = lib.mkOption { type = lib.types.int; };
+        bar = lib.mkOption { type = lib.types.str; };
+      };
+    };
+  };
+
+  options.sub = lib.mkOption {
+    type = lib.types.attrsOf (
+      lib.types.submodule {
+        options = {
+          foo = lib.mkOption { type = lib.types.int; };
+          bar = lib.mkOption { type = lib.types.str; };
+        };
+      }
+    );
+  };
 
   config.msg = "hello world";
   config.test = "${test-module-arg}";
   config.path = "${pkgs.hello}/bin/hello";
   config.test2 = "${test-specialArg}";
+
+  config.subraw = {
+    foo = 1;
+    bar = "hello";
+  };
+
+  config.sub = {
+    a = {
+      foo = 1;
+      bar = "hello";
+    };
+  };
 }
