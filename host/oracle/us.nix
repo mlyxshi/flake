661757d31bc855ce90f844d5aaecb1d9917f8a29
mlyxshi <mlyxdev@gmail.com>
2026-06-30 23:39:57 +0800

    update

--- host/oracle/us.nix
+++ host/oracle/us.nix
@@ -6,18 +6,21 @@
   ...
 }:
 let
-    old-pkgs = import (builtins.fetchTarball {
-        url = "https://github.com/NixOS/nixpkgs/archive/e6f23dc08d3624daab7094b701aa3954923c6bbb.tar.gz";
-    }) {};
+  old-pkgs = import (builtins.fetchGit {
+    name = "my-old-revision";
+    url = "https://github.com/NixOS/nixpkgs/";
+    ref = "refs/heads/nixpkgs-unstable";
+    rev = "e6f23dc08d3624daab7094b701aa3954923c6bbb";
+  }) { };
 
-    sing-box-stable = old-pkgs.sing-box;
+  sing-box-stable = old-pkgs.sing-box;
 in
 {
 
   imports = [
     self.nixosModules.programs.vscode-ssh-remote
   ];
-  
+
   boot.kernel.sysctl = {
     "net.ipv4.ip_forward" = 1;
   };
