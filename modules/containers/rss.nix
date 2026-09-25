cd1347e7e1291b7334fcd8c6c3ccf95332b0a2ee
mlyxdev <mlyxdev@gmail.com>
2026-09-26 01:24:23 +0800

    update

--- modules/containers/rss.nix
+++ modules/containers/rss.nix
@@ -21,12 +21,7 @@
   virtualisation.oci-containers.containers.miniflux = {
     image = "ghcr.io/miniflux/miniflux";
     dependsOn = [ "miniflux-postgres" ];
-    environmentFiles = [ /secret/miniflux-oidc ];
     environment = {
-      OAUTH2_PROVIDER = "oidc";
-      OAUTH2_REDIRECT_URL = "https://miniflux.${config.networking.domain}/oauth2/oidc/callback";
-      OAUTH2_OIDC_DISCOVERY_ENDPOINT = "https://sso.${config.networking.domain}";
-      OAUTH2_USER_CREATION = "1";
       # DISABLE_LOCAL_AUTH = "1";
       # CREATE_ADMIN = "1";
       RUN_MIGRATIONS = "1";
